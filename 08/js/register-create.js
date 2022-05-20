2685ab13ce66ecb9056fc66096ab20e53f8c656d
TEST4613 <TEST4613@mail.com>
2022-05-20 17:45:20 +0900

    220520_ver

--- 08/js/register-create.js
+++ 08/js/register-create.js
@@ -5,16 +5,31 @@ btn.addEventListener("click",function(){
     var newP = document.createElement("p")
     var newText = document.createTextNode(input_box.value)
     newP.appendChild(newText)
-    document.body.appendChild(newP)
+    //document.body.appendChild(newP)
     newP.setAttribute("class","accent")
 
     //
     var delBtn = document.createElement("span");
     var delText = document.createTextNode("X");
     delBtn.setAttribute("class","del");
+    //delBtn.setAttribute("onclick",alert('ddd'));
+    //onclick은 attribute가 아니라고 한다
+    
+    // delBtn.onclick=function(){
+    //     alert('신청을 취소합니다.')
+    //     //console.log(this.parentNode);
+    //     //console.log(this.parentNode.parentNode);
+    //     if(this.parentNode.parentNode)
+	// 	    this.parentNode.parentNode.removeChild(this.parentNode)
+    // };
+
+    delBtn.addEventListener("click",function(){
+        alert('신청을 취소합니다.')
+        if(this.parentNode.parentNode)
+		    this.parentNode.parentNode.removeChild(this.parentNode)
+    });
     delBtn.appendChild(delText);
     newP.appendChild(delBtn);
-
     //
 
     input_box.value = "";
@@ -23,4 +38,7 @@ btn.addEventListener("click",function(){
     //div1.appendChild(newP);
     div1.insertBefore(newP,div1.childNodes[0]);
 
+
 })
+
+
