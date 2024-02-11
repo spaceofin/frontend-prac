77588baa117550e3ada9ce437dc31468d921eec3
spaceofin <ineemig@gmail.com>
2024-02-11 17:39:55 +0900

    [react-tutorial-self-practice]feat: add XO mark on click

--- react-tutorial-self-practice/src/index.js
+++ react-tutorial-self-practice/src/index.js
@@ -4,24 +4,43 @@ import './index.css';
 
 class Square extends React.Component {
     render() {
-        const a = "hi";
         return (
-            <button className="square" >
-                {a}
+            <button className="square" onClick={this.props.onClick}>
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
-    handleClick() {
-        console.log('hi~');
+    constructor(props) {
+        super(props);
+        this.state = {
+            isX: true,
+            squares: Array(9).fill(null),
+        }
+    }
+
+    handleClick(i) {
+        //console.log(this.state.isX ? 'X' : 'O');
+        const squares = this.state.squares.slice();
+        squares[i] = (this.state.isX ? 'X' : 'O');
+        this.setState({
+            isX: !this.state.isX,
+            squares: squares,
+        })
     }
 
     render() {
-        const squares = [];
+        const squares = this.state.squares.slice();
         for (let i = 0; i < 9; i++) {
-            squares.push(<Square key={i} />)
+            squares[i] =
+                <Square
+                    key={i}
+                    isX={this.state.isX}
+                    value={this.state.squares[i]}
+                    onClick={() => this.handleClick(i)}
+                />
         }
         return (
             <div className="squares">
