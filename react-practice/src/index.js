4a9c415677bbe8b7af5187fd39cd6e9091c5daa1
spaceofin <ineemig@gmail.com>
2024-02-14 22:33:39 +0900

    [react-practice]update: practice javascript expressions in JSX and ES6 template literals

--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const firstName = "A";
+const lastName = "B";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+ReactDOM.render(
+    <div>
+        <h1>{firstName + " " + lastName}'s Favoriate Foods</h1>
+        {/* <h1>{firstName} {lastName}'s Favoriate Foods</h1> */}
+        {/* <h1>{`${firstName} ${lastName}`}'s Favoriate Foods</h1> */}
+        <ul>
+            <li>Bacon</li>
+            <li>Jamon</li>
+            <li>Noodles</li>
+        </ul>
+        <p>Your lucky number is {Math.floor(Math.random() * 10)}</p>
+    </div>,
+    document.getElementById("root")
+);
