7094e2a136b2c9df5bbe07d2db4abc6d38e98bc7
spaceofin <ineemig@gmail.com>
2024-02-15 14:11:42 +0900

    [react-moster-slayer]feat: add header and hp bar appearance

--- react-monster-slayer/src/index.js
+++ react-monster-slayer/src/index.js
@@ -2,11 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const App = (
-    <div>
-        Monster Slayer Game
-    </div>
-);
+class Game extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="header">
+                    Monster Slayer
+                </div>
+                <div>
+                    <div className="hp-box">
+                        Monster
+                        <div className="hp">
+                            <div className="hp-gauge" style={{ width: '25%' }} />
+                        </div>
+                    </div>
+                    <div className="hp-box">
+                        Player
+                        <div className="hp">
+                            <div className="hp-gauge" style={{ width: '50%' }} />
+                        </div>
+                    </div>
+
+                </div>
+            </div >
+        );
+    }
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(App);
+root.render(<Game />);
