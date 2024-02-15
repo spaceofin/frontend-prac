cb332485bc3b1b572f3500e0726d1205a41ecac1
spaceofin <ineemig@gmail.com>
2024-02-15 20:56:54 +0900

    [react-monster-slayer]feat: add skill counts, reset button and battle logs

--- react-monster-slayer/src/index.js
+++ react-monster-slayer/src/index.js
@@ -8,6 +8,9 @@ class Game extends React.Component {
         this.state = {
             monsterHP: 100,
             playerHP: 100,
+            magicAttackCount: 2,
+            healCount: 2,
+            battleLogs: [],
         };
     }
 
@@ -22,27 +25,61 @@ class Game extends React.Component {
         )
     }
 
-    skillBox(skillName) {
+    skillBox(skillName, count) {
         return (
-            <button onClick={() => this.useSkill(skillName)}>{skillName}</button>
+            <button onClick={() => this.useSkill(skillName)}>{count != null ? `${skillName} : ${count}` : skillName}</button>
         )
     }
 
     useSkill(skillName) {
+
+        if (skillName === "RESET") {
+            this.setState({
+                monsterHP: 100,
+                playerHP: 100,
+                magicAttackCount: 2,
+                healCount: 2,
+                battleLogs: [],
+            });
+            return;
+        }
+
+        if (this.state.monsterHP <= 0 || this.state.playerHP <= 0) {
+            return;
+        }
+
+        if ((skillName === "MAGIC ATTACK" && this.state.magicAttackCount === 0) ||
+            (skillName === "HEAL" && this.state.healCount === 0)) {
+            return;
+        }
+
         let attackValue = 0;
         let attackedValue = 0;
         let healValue = 0;
+        let isMagicAttak = false;
+        let isHeal = false;
+
+
+        const battleLogs = this.state.battleLogs.slice();
+
         switch (skillName) {
             case 'ATTACK':
                 attackValue = Math.floor(Math.random() * 11) + 10;
                 attackedValue = Math.floor(Math.random() * 16) + 10;
+                battleLogs.push(`Player attaked Monster for ${attackValue} damage`);
+                battleLogs.push(`Monster attaked Player for ${attackedValue} damage`);
                 break;
             case 'MAGIC ATTACK':
                 attackValue = Math.floor(Math.random() * 21) + 20;
                 attackedValue = Math.floor(Math.random() * 16) + 15;
+                battleLogs.push(`Player attaked Monster for ${attackValue} damage`);
+                battleLogs.push(`Monster attaked Player for ${attackedValue} damage`);
+                isMagicAttak = true;
                 break;
             case 'HEAL':
                 healValue = Math.floor(Math.random() * 6) + 15;
+                battleLogs.push(`Player healed for ${healValue} points`);
+                isHeal = true;
                 break;
             default:
                 break;
@@ -53,7 +90,10 @@ class Game extends React.Component {
 
         this.setState({
             monsterHP: this.state.monsterHP - attackValue < 0 ? 0 : this.state.monsterHP - attackValue,
-            playerHP: this.state.playerHP - attackedValue + healValue < 0 ? 0 : this.state.playerHP - attackedValue + healValue,
+            playerHP: Math.min(100, this.state.playerHP - attackedValue + healValue < 0 ? 0 : this.state.playerHP - attackedValue + healValue),
+            battleLogs: battleLogs,
+            magicAttackCount: this.state.magicAttackCount - (isMagicAttak ? 1 : 0),
+            healCount: this.state.healCount - (isHeal ? 1 : 0),
         });
     }
 
@@ -63,11 +103,11 @@ class Game extends React.Component {
         let status = "Battle Going On";
 
         if (this.state.monsterHP <= 0 && this.state.playerHP <= 0) {
-            status = 'ALL DIE';
+            status = 'ALL DIE, NO ONE WIN.';
         } else if (this.state.playerHP <= 0) {
-            status = 'PLAYER DIE';
+            status = 'PLAYER DIE, MONSTER WIN.';
         } else if (this.state.monsterHP <= 0) {
-            status = 'MOSTER DIE';
+            status = 'MOSTER DIE, PLAYER WIN.';
         }
 
         return (
@@ -81,12 +121,17 @@ class Game extends React.Component {
                 </div>
                 <div className="button-panel">
                     {this.skillBox('ATTACK')}
-                    {this.skillBox('MAGIC ATTACK')}
-                    {this.skillBox('HEAL')}
+                    {this.skillBox('MAGIC ATTACK', this.state.magicAttackCount)}
+                    {this.skillBox('HEAL', this.state.healCount)}
                     {this.skillBox('RESET')}
                 </div>
                 <div className="status">
                     {status}
+                </div>
+                <div className="battle-logs">
+                    {this.state.battleLogs.map((log, i) => (
+                        <div key={i}>{log}</div>
+                    ))}
                 </div>
             </div >
         );
