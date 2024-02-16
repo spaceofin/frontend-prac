4b9c16893fe029bed6f514aa4ba19e0d4b094026
spaceofin <ineemig@gmail.com>
2024-02-16 22:33:18 +0900

    [react-syntax-practice]update: practice JSX

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,28 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-
-function getFullName(user) {
-    return `${user.firstName} ${user.lastName}`;
-}
-
-function getHobby(user) {
-    return <h1>{getFullName(user)}'s hobby is {user.hobby}!</h1>;
-}
-
-const user = {
-    firstName: 'A',
-    lastName: 'a',
-    hobby: 'coding',
-}
-
-
-const app = (
-    <div>
-        {getHobby(user)}
-    </div>
-)
+import UserProfile from './UserProfile';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(app);
+root.render(<UserProfile />);
