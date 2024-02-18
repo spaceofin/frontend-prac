49a9f0db77702a36630b6d8a0384a0c259b12402
spaceofin <ineemig@gmail.com>
2024-02-18 22:38:42 +0900

    [react-syntax-practice]update: practice hook - useEffect

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,9 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+// class CountNumber extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             i: 0
+//         }
+//     }
+
+//     componentDidMount() {
+//         document.title = `You clicked ${this.state.i} times`;
+//     }
+//     componentDidUpdate() {
+//         document.title = `You clicked ${this.state.i} times`;
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={() => this.setState({
+//                     i: this.state.i + 1
+//                 })}>
+//                     클릭하세요
+//                 </button>
+//                 <div>클릭 수: {this.state.i}</div>
+//             </div >
+//         );
+//     }
+// }
+
 function CountNumber(props) {
     const [i, setI] = useState(0);
+
+    useEffect(() => {
+        document.title = `You cliked ${i} times`;
+    });
 
     return (
         <div>
@@ -13,7 +45,7 @@ function CountNumber(props) {
             <div>클릭 수: {i}</div>
         </div>
     );
-
 }
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<CountNumber />);
