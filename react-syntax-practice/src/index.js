cbc61f2aceebc6f525ac6e78273496c1d603bf1c
spaceofin <ineemig@gmail.com>
2024-02-18 19:34:47 +0900

    [react-syntax-practice]update: practice hook - useState

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import UserProfile from './UserProfile';
-import Notifications from './Notifications';
 
+function CountNumber(props) {
+    const [i, setI] = useState(0);
+
+    return (
+        <div>
+            <button onClick={() => setI(i + 1)}>
+                클릭하세요
+            </button>
+            <div>클릭 수: {i}</div>
+        </div>
+    );
+
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Notifications />);
+root.render(<CountNumber />);
