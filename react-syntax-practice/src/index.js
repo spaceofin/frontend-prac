0d032e190527e8d00f0a242abdba2e0a7fb955a0
spaceofin <ineemig@gmail.com>
2024-02-21 17:15:26 +0900

    [react-syntax-practice]update: practice hook - useMemo

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,37 +1,51 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
-
 function NumberSum(props) {
     const [numArr, setNumArr] = useState(props.numArr);
-    const [inputValue, setInputValue] = useState(null);
+    const [prevValue, setPrevValue] = useState(null);
 
     // const [arrSum, setArrSum] = useState(null);
     // useEffect(() => {
+    //     console.log("useEffect hook runs");
     //     setArrSum(numArr.reduce((accumulator, currentValue) => accumulator + currentValue, 0));
     // }, [numArr]);
 
     const arrSum = useMemo(() => {
+        console.log("useMemo hook runs");
         return numArr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
     }, [numArr]);
 
-    const addNumToArr = () => {
+
+    const addNumToArr = (inputValue) => {
+        if (inputValue === null) {
+            inputValue = prevValue;
+        }
+        console.log(`inputValue: ${inputValue}`);
+        setPrevValue(inputValue);
+
+        if (inputValue >= 10) {
+            return;
+        }
         const parsedValue = parseInt(inputValue);
         if (!isNaN(parsedValue)) {
             setNumArr([...numArr, parsedValue]);
-            console.log(`inputValue: ${inputValue}`);
-            console.log(`numArr: [ ${numArr.join(' ')} ${parsedValue} ]`);
+            // console.log(`inputValue: ${inputValue}`);
+            // console.log(`numArr: [ ${numArr.join(' ')} ${parsedValue} ]`);
         }
     }
+
+    let enteredValue = null;
 
     return (
         <div>
             <p>Number array : [ {numArr.map(num => num + ' ')} ] </p>
             <p>Sum of array : {arrSum}</p>
-            <input onChange={(e) => { setInputValue(e.target.value); }} />
-            <button onClick={addNumToArr}>Enter</button>
+            <p>Previous input value : {prevValue}</p>
+            <input onChange={(e) => { enteredValue = e.target.value; }} />
+            <button onClick={() => { addNumToArr(enteredValue) }}>Enter</button>
         </div>
     )
 }
