456d6d4990093e5197b2972ea8470dacac0b43ba
spaceofin <ineemig@gmail.com>
2024-02-22 11:57:43 +0900

    [react-practice]update: practice bootstrap framework

--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
 
-const firstName = "A";
-const lastName = "B";
+// const firstName = "A";
+// const lastName = "B";
 
-const App = (
-    <div>
-        <h1>{firstName + " " + lastName}'s Favoriate Foods</h1>
-        {/* <h1>{firstName} {lastName}'s Favoriate Foods</h1> */}
-        {/* <h1>{`${firstName} ${lastName}`}'s Favoriate Foods</h1> */}
-        <ul>
-            <li>Bacon</li>
-            <li>Jamon</li>
-            <li>Noodles</li>
-        </ul>
-        <p>Your lucky number is {Math.floor(Math.random() * 10)}</p>
-    </div>
-);
+// const App = (
+//     <div>
+//         <h1>{firstName + " " + lastName}'s Favoriate Foods</h1>
+//         {/* <h1>{firstName} {lastName}'s Favoriate Foods</h1> */}
+//         {/* <h1>{`${firstName} ${lastName}`}'s Favoriate Foods</h1> */}
+//         <ul>
+//             <li>Bacon</li>
+//             <li>Jamon</li>
+//             <li>Noodles</li>
+//         </ul>
+//         <p>Your lucky number is {Math.floor(Math.random() * 10)}</p>
+//     </div>
+// );
 
-const root = createRoot(document.getElementById("root"));
-root.render(App);
+// const root = createRoot(document.getElementById("root"));
+// root.render(App);
