eff24a0a5415110aeba592cb54fc9bcfddd75fda
spaceofin <ineemig@gmail.com>
2024-02-29 23:46:13 +0900

    [react-syntax-practice]update: practice event-handling with arguments

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,60 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 
-
-function useCounter(initialValue) {
-    const [count, setCount] = useState(initialValue);
-
-    const increaseCount = () => setCount((count) => count + 1);
-    const decreaseCount = () => setCount((count) => Math.max(count - 1, 0));
-
-    return [count, increaseCount, decreaseCount];
+const btnMargin = {
+    margin: '5px',
 }
-
-const MAX_CAPACITY = 5;
-
-function RoomEntry(props) {
-    const [isFull, setIsFull] = useState(false);
-    const [occupancy, increaseOccupancy, decreaseOccupancy] = useCounter(props.room.occupancy);
-
-    useEffect(() => {
-        setIsFull(occupancy >= MAX_CAPACITY);
-        console.log(`Room${props.room.id} occupancy: ${occupancy}`);
-    }, [occupancy, props.room.id]);
-
-    return (
-        <div className="room">
-            <p className="room-title">Room {props.room.id}</p>
-            <p className="room-occupancy">occupancy: {occupancy}/5</p>
-            <div className="check-buttons">
-                <button onClick={increaseOccupancy} disabled={isFull}>CHECK-IN</button>
-                <button onClick={decreaseOccupancy} disabled={occupancy === 0}>CHECK-OUT</button>
-            </div>
-            {occupancy === 0 && <p>Room {props.room.id} is Empty</p>}
-            {isFull && <p>Room {props.room.id} is Full</p>}
-        </div >
-    )
-
+const lastClickedButtonInfo = {
+    marginLeft: '10px',
+    fontFamily: 'sans-serif',
+    fontWeight: 'bold',
+    color: 'navy',
 }
 
 const App = () => {
+    const [buttonId, setButtonId] = useState('No button clicked');
+    const [count, setIsCount] = useState(1);
 
-    const rooms = [
-        { id: 1, occupancy: 0, isFull: false },
-        { id: 2, occupancy: 0, isFull: false },
-        { id: 3, occupancy: 0, isFull: false },
-    ];
+    const handleClick = (buttonId, event) => {
+        setButtonId(buttonId);
+        setIsCount(count + 1);
+        console.log(`event.target: ${event.target} event.target.tagName: ${event.target.tagName}`);
+    }
 
     return (
-        <div className="rooms">
-            {rooms.map(room => (
-                <RoomEntry key={room.id} room={room} />
-            ))}
+        <div>
+            <button style={btnMargin} onClick={(event) => handleClick('A', event)}>button A</button>
+            <button style={btnMargin} onClick={(event) => handleClick('B', event)}>button B</button>
+            <button style={btnMargin} onClick={(event) => handleClick('C', event)}>button C</button>
+            <p style={lastClickedButtonInfo}>Last Clicked button: {buttonId}</p>
         </div>
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-
