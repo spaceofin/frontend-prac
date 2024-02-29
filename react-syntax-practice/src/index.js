9665f014b039c1342d7ea87a0d3f2cc4afb06956
spaceofin <ineemig@gmail.com>
2024-02-29 13:41:47 +0900

    [react-syntax-practice]update: practice components - function and class components

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,54 +1,44 @@
-// import React, { useState } from 'react';
-// import ReactDOM from 'react-dom/client';
-
-// function App() {
-//     const [isButtonOn, setIsButtonOn] = useState(true);
-//     const [product, setProduct] = useState(1);
-//     const [enteredValue, setEnteredValue] = useState(1);
-
-//     const handleClick = () => {
-//         setIsButtonOn(prevState => !prevState);
-//     }
-
-//     const handleChange = (e) => {
-//         const inputValue = parseInt(e.target.value);
-//         if (isNaN(inputValue)) {
-//             console.log("The entered value is not number.");
-//             return;
-//         }
-//         setEnteredValue(inputValue);
-//     }
-
-//     return (
-//         < div >
-//             <button onClick={handleClick}>
-//                 {isButtonOn ? 'On' : 'Off'}
-//             </button>
-//             <br /><br />
-//             <p>{`Product : ${product}`}</p>
-//             <p>Enter the number want to multiply</p>
-//             <input onChange={handleChange} />
-//             <button onClick={(e) => setProduct(product * enteredValue)}>
-//                 enter
-//             </button>
-//         </div>
-//     )
-
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<App />);
-
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Clock from './Clock';
-import './index.css';
+import ReactDOM from 'react-dom/client';
 
-setInterval(() => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <Clock />
-        </React.StrictMode>,
-        document.getElementById('root')
+
+class ClassComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>This is Class Component.</p>
+            </div>
+        );
+    }
+}
+
+const FunctionComponentA = () => {
+    return (
+        <div>
+            <p>This is Function Component Type A.</p>
+        </div>
     );
-}, 1000);
+}
+
+function FunctionComponentB() {
+    return (
+        <div>
+            <p>This is Function Component Type B.</p>
+        </div>
+    );
+}
+
+
+const App = () => {
+    return (
+        <div>
+            <ClassComponent />
+            <FunctionComponentA />
+            <FunctionComponentB />
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
