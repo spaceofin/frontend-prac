7380e8458b84566055b8603068364940dc01e030
spaceofin <ineemig@gmail.com>
2024-02-29 17:16:08 +0900

    [react-syntax-practice]update: practice customHook - RoomEntry using useCounter customHook

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,40 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 
-class ClassComponent extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>This is {this.props.componentType}.</p>
+function useCounter(initialValue) {
+    const [count, setCount] = useState(initialValue);
+
+    const increaseCount = () => setCount((count) => count + 1);
+    const decreaseCount = () => setCount((count) => Math.max(count - 1, 0));
+
+    return [count, increaseCount, decreaseCount];
+}
+
+const MAX_CAPACITY = 5;
+
+function RoomEntry(props) {
+    const [isFull, setIsFull] = useState(false);
+    const [occupancy, increaseOccupancy, decreaseOccupancy] = useCounter(props.room.occupancy);
+
+    useEffect(() => {
+        setIsFull(occupancy >= MAX_CAPACITY);
+        console.log(`Room${props.room.id} occupancy: ${occupancy}`);
+    }, [occupancy, props.room.id]);
+
+    return (
+        <div className="room">
+            <p className="room-title">Room {props.room.id}</p>
+            <p className="room-occupancy">occupancy: {occupancy}/5</p>
+            <div className="check-buttons">
+                <button onClick={increaseOccupancy} disabled={isFull}>CHECK-IN</button>
+                <button onClick={decreaseOccupancy} disabled={occupancy === 0}>CHECK-OUT</button>
             </div>
-        );
-    }
-}
+            {occupancy === 0 && <p>Room {props.room.id} is Empty</p>}
+            {isFull && <p>Room {props.room.id} is Full</p>}
+        </div >
+    )
 
-const FunctionComponentA = (props) => {
-    return (
-        <div>
-            <p>This is {props.componentType} Type A.</p>
-        </div>
-    );
 }
-
-function FunctionComponentB(props) {
-    return (
-        <div>
-            <p>This is {props.componentType} Type B.</p>
-        </div>
-    );
-}
-
 
 const App = () => {
+
+    const rooms = [
+        { id: 1, occupancy: 0, isFull: false },
+        { id: 2, occupancy: 0, isFull: false },
+        { id: 3, occupancy: 0, isFull: false },
+    ];
+
     return (
-        <div>
-            <ClassComponent componentType="Class Component" />
-            <FunctionComponentA componentType="Function Component" />
-            <FunctionComponentB componentType="Function Component" />
+        <div className="rooms">
+            {rooms.map(room => (
+                <RoomEntry key={room.id} room={room} />
+            ))}
         </div>
     )
 }
