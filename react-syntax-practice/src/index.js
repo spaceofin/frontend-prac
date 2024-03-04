05fee2bb42034165b96b42393c1ba2e05989d1b3
spaceofin <ineemig@gmail.com>
2024-03-05 03:01:46 +0900

    [react-syntax-practice]update: practice form - multi inputs

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -6,12 +6,16 @@ const styles = {
         display: 'inline-block',
         margin: '30px',
         width: '350px',
+        height: '130px',
         padding: '15px 30px 15px 50px',
         backgroundColor: ' #FFD700',
         borderRadius: '10px',
+        position: 'relative',
     },
     submitButton: {
+        position: 'absolute',
         marginLeft: '2px',
+        bottom: '20px',
     },
     nickname: {
         fontFamily: 'Arial',
@@ -25,11 +29,19 @@ const styles = {
 }
 
 const App = () => {
-    const [feeling, setFeeling] = useState('happy');
-    const [weather, setWeather] = useState('sunny');
+    const [needSandwich, setNeedSandwich] = useState(false);
+    const [count, setCount] = useState(1);
 
     const handleSubmit = (event) => {
-        alert('Today\'s weather was ' + weather + ',\nand your day was ' + feeling + '.\n\nHave a good dream.');
+        if (needSandwich) {
+            if (count == 1) {
+                alert('You need a sandwich.');
+            } else {
+                alert('You need ' + count + ' sandwiches.');
+            }
+        } else {
+            alert('You don\'t need a sandwich.')
+        }
         event.preventDefault();
     }
 
@@ -37,27 +49,32 @@ const App = () => {
         <div style={styles.wrapper}>
             <form onSubmit={handleSubmit}>
                 <label>
-                    <p style={styles.question}>What was the weather like today?</p>
-                    <select style={styles.selectItems} value={weather} onChange={(event) => { setWeather(event.target.value) }} >
-                        <option value="sunny">sunny</option>
-                        <option value="cloudy">cloudy</option>
-                        <option value="rainy">rainy</option>
-                        <option value="snowy">snowy</option>
-                        <option value="etc.">etc.</option>
-                    </select>
+                    <input
+                        type="checkbox"
+                        checked={needSandwich}
+                        onChange={(event) => { setNeedSandwich(event.target.checked) }}
+                    />
+                    Check if you need sandwiches.
                 </label>
-                <label>
-                    <p style={styles.question}>How was your day?</p>
-                    <select style={styles.selectItems} value={feeling} onChange={(event) => { setFeeling(event.target.value) }} >
-                        <option value="happy">happy</option>
-                        {/* <option selected value="happy">happy</option> */}
-                        <option value="sad">sad</option>
-                        <option value="bored">bored</option>
-                        <option value="excited">excited</option>
-                        <option value="grateful">grateful</option>
-                    </select>
-                </label>
-                <br /><br />
+                <br />
+                <br />
+                {needSandwich &&
+                    <label>
+                        How many sandwiches do you need?
+                        <input
+                            type="number"
+                            value={count}
+                            onChange={(event) => {
+                                let inputValue = event.target.value;
+                                if (inputValue >= 1) {
+                                    setCount(inputValue);
+                                }
+                            }}
+                        />
+                    </label>
+                }
+                <br />
+                <br />
                 <button style={styles.submitButton} type="submit">submit</button>
             </form >
         </div >
