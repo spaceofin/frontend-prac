7c44f05d66d89402989e8b62a60d9e275de48cdd
spaceofin <ineemig@gmail.com>
2024-03-06 16:59:53 +0900

    [react-syntax-practice]update: practice form - react-select package multiselect

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -2,18 +2,44 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import Select from 'react-select';
 
+const styles = {
+    wrapper: {
+        display: 'inline-block',
+        margin: '30px',
+        width: '350px',
+        height: '50px',
+        padding: '30px 30px 30px 30px',
+        backgroundColor: ' #FFD700',
+        borderRadius: '10px',
+        justifyContent: 'center',
+    },
+    submitButton: {
+        position: 'absolute',
+        bottom: '20px',
+    },
+    selectItems: {
+        position: 'absolute',
+        marginTop: '10px',
+        width: '70px',
+        fontSize: '15px',
+    }
+}
+
+
 const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
+    { value: 'A', label: 'A' },
+    { value: 'B', label: 'B' },
+    { value: 'C', label: 'C' },
+    { value: 'D', label: 'D' },
 ];
 
 function App() {
     const [selectedOption, setSelectedOption] = useState(null);
 
     return (
-        <div className="App">
+        <div style={styles.wrapper}>
             <Select
+                isMulti
                 defaultValue={selectedOption}
                 onChange={setSelectedOption}
                 options={options}
