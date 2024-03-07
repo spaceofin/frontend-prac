92b937aebd3529ff134f4a036c5620cc898c0974
spaceofin <ineemig@gmail.com>
2024-03-08 01:11:55 +0900

    [react-syntax-practice]update: practice form - input default value and re-entering value

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<input value="hi" />);
 
-setTimeout(function () {
-    ReactDOM.createRoot(document.getElementById('root')).render(<input value={null} />);
-}, 1000);
+const App = () => {
+    const [inputValue, setInputValue] = useState('hi');
+    const [isDisabled, setIsDisabled] = useState(true);
+    const [showButton, setShowButton] = useState(false);
+    const [showInputValue, setShowInputValue] = useState(false);
+
+    useEffect(() => {
+        setTimeout(function () {
+            setInputValue('');
+            setIsDisabled(false);
+            setShowButton(true);
+        }, 2000);
+    }, []);
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleClick = () => {
+        setShowInputValue(true);
+    }
+
+    const handleFocus = () => {
+        setShowInputValue(false);
+    }
+
+    return (
+        <div>
+            <input
+                value={inputValue}
+                onChange={handleChange}
+                onFocus={handleFocus}
+                disabled={isDisabled} />
+            {showButton && <button onClick={handleClick}>Enter</button>}
+            <br />
+            {showInputValue && inputValue}
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />)
