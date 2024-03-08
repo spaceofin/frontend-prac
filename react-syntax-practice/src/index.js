a1ab8d5aba33877a4351838bfd138d81ac3c58f0
spaceofin <ineemig@gmail.com>
2024-03-09 00:19:44 +0900

    [react-syntax-practice]update: practice form - signup example

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 
 function SignUp(props) {
     const [id, setId] = useState("");
+    const [teamColor, setTeamColor] = useState("RED");
 
     const handleChangeId = (event) => {
         setId(event.target.value);
     };
 
     const handleSubmit = (event) => {
-        alert(`ID: ${id}`);
+        alert(`ID: ${id}\nTEAM COLOR: ${teamColor}`);
         event.preventDefault();
     };
+
+    const handleChangeTeamColor = (event) => {
+        setTeamColor(event.target.value);
+    }
 
     return (
         <form onSubmit={handleSubmit}>
@@ -19,6 +24,17 @@ function SignUp(props) {
                 ID
                 <input type="text" value={id} onChange={handleChangeId} />
             </label>
+            <br />
+            <label>
+                TEAM COLOR
+                <select value={teamColor} onChange={handleChangeTeamColor}>
+                    <option value="RED">RED</option>
+                    <option value="YELLOW">YELLOW</option>
+                    <option value="GREEN">GREEN</option>
+                    <option value="BLUE">BLUE</option>
+                </select>
+            </label>
+            <br />
             <button type="submit">summit</button>
         </form>
     );
