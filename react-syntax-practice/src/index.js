2f2853cdd1790894e261a4a9e2455990f39f97f5
spaceofin <ineemig@gmail.com>
2024-03-15 20:05:02 +0900

    [react-syntax-practice]update: composition practice - containment

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,98 +1,33 @@
-import React, { useState } from "react";
+
+import React from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-function CityTime(props) {
-    const handleChange = (event) => {
-        const inputTime = event.target.value.trim();
-
-        if (inputTime === '') {
-            props.onTimeChange(props.city, '');
-            return;
-        }
-
-        const time = parseInt(inputTime);
-
-        if (time < 0 || time >= 24) {
-            alert('Enter a time between 0 and 23');
-            props.onTimeChange(props.city, '');
-        } else {
-            props.onTimeChange(props.city, time);
-            console.log(`city: ${props.city}, time: ${time}`)
-        }
-    };
-
-    const inputId = `${props.city.toLowerCase()}TimeInput`;
-
+function ColorBox(props) {
     return (
-        <fieldset className="fieldset-city">
-            <legend>
-                {props.city} Time
-            </legend>
-            <input className="input-time" id={inputId} type="number" value={isNaN(props.time) ? '' : props.time} onChange={handleChange} placeholder="Enter hour(0-23)" />
-            <span className="span-oclock">O'clock</span>
-        </fieldset>
-    )
-}
-
-function convertTime(inputCity, inputTime, targetCity) {
-    if (inputTime === '') {
-        return '';
-    }
-
-    const timeDifference = {
-        Seoul: 0,
-        Singapore: -1,
-        Hawaii: 5
-    };
-
-    const diff = timeDifference[targetCity] - timeDifference[inputCity];
-    return (inputTime + 24 + diff) % 24;
-}
-
-function TimeDifference(props) {
-    const [inputCity, setInputCity] = useState('');
-    const [time, setTime] = useState('');
-
-    const seoulTime = convertTime(inputCity, time, "Seoul");
-    const singaporeTime = convertTime(inputCity, time, "Singapore");
-    const hawaiiTime = convertTime(inputCity, time, "Hawaii");
-
-    const handleChange = (city, time) => {
-        setInputCity(city);
-        if (time === '') {
-            setTime('');
-        } else {
-            setTime(time);
-        }
-        console.log(`inputCity: ${city}, inputTime: ${time}`);
-    }
-
-    return (
-        <div className="div-citytime">
-            <CityTime
-                city="Seoul"
-                time={seoulTime}
-                onTimeChange={handleChange}
-            />
-            <CityTime
-                city="Singapore"
-                time={singaporeTime}
-                onTimeChange={handleChange}
-            />
-            <CityTime
-                city="Hawaii"
-                time={hawaiiTime}
-                onTimeChange={handleChange}
-            />
+        <div className={'colorbox-' + props.color}>
+            {props.children}
         </div>
-    )
+    );
+}
+
+function ColorPanel(props) {
+    return (
+        <div>
+            <ColorBox color="orange">
+                <p>Orange</p>
+            </ColorBox>
+            <ColorBox color="blue">
+                <p>Blue</p>
+            </ColorBox>
+        </div>
+    );
 }
 
 const App = () => {
     return (
         <div>
-            <TimeDifference />
+            <ColorPanel />
         </div>
     );
 }
