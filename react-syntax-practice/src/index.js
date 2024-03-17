ceeb8d1dcf501a653f1f3459be5a6f38734c54e1
spaceofin <ineemig@gmail.com>
2024-03-17 23:33:38 +0900

    [react-syntax-practice]update: context practice - without using context

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,61 +1,60 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-function Card(props) {
-    const { name, backgroundColor, children } = props;
-
+// The Toolbar component must take an extra "theme" prop  
+// and pass it to the ThemedButton
+function Toolbar(props) {
     return (
-        <div
-            style={{
-                width: 250,
-                padding: 10,
-                paddingLeft: 30,
-                borderRadius: 10,
-                boxShadow: "2px 2px 2px black",
-                backgroundColor: backgroundColor || "white",
-            }}
-        >
-            {name && <h1>{name}</h1>}
-            {children && <div style={({ fontSize: 20, fontFamily: "sans-serif", })}>{children}</div>}
-        </div >
-    );
-}
-
-function ProfileCard(props) {
-    return (
-        <div
-            style={{
-                display: "grid",
-                gridTemplateColumns: "repeat(2,300px)",
-                gap: 20,
-                margin: 50,
-                marginTop: 70,
-            }}
-        >
-            <Card name="Mary" backgroundColor="lightblue">
-                <p>Project Manager</p>
-            </Card>
-            <Card name="Jake" backgroundColor="lightcoral">
-                <p>Engineer</p>
-            </Card>
-            <Card name="Ashley" backgroundColor="lightSteelBlue">
-                <p>Backend developer</p>
-            </Card>
-            <Card name="Christopher" backgroundColor="lightSalmon">
-                <p>Desinger</p>
-            </Card>
-            <Card name="Lauren" backgroundColor="lightGray">
-                <p>Frontend Developer</p>
-            </Card>
+        <div>
+            <ThemedButton theme={props.theme} />
         </div>
     );
 }
 
+function ThemedButton(props) {
+    return (
+        <Button theme={props.theme} />
+    )
+}
+
+function Button(props) {
+    let buttonTheme = {};
+
+    switch (props.theme) {
+        case 'dark':
+            buttonTheme = { backgroundColor: 'black', color: 'white' };
+            break;
+        case 'light':
+            buttonTheme = { backgroundColor: 'white', color: 'black' };
+            break;
+        case 'green':
+            buttonTheme = { backgroundColor: 'green', color: 'yellow' };
+            break;
+        default:
+            buttonTheme = { backgroundColor: 'white', color: 'black' };
+    }
+
+    return (
+        <button
+            style={buttonTheme}
+        >
+            BUTTON
+        </button>
+    )
+}
+
 const App = () => {
     return (
-        <ProfileCard />
+        <div>
+            <Toolbar theme="dark" />
+            <Toolbar theme="light" />
+            <Toolbar theme="green" />
+            <Toolbar />
+        </div>
     );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />)
+
+
