8c98633370cc10eb84bfcb67cb8e108680cdaa1f
spaceofin <ineemig@gmail.com>
2024-03-18 19:03:33 +0900

    [react-syntax-practice]update: practice styling - css selector

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,60 +1,29 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
-
-function Toolbar() {
-    return (
-        <div>
-            <ThemedButton />
-        </div>
-    );
-}
-
-function ThemedButton() {
-    return (
-        <ThemeContext.Consumer>
-            {value => <Button theme={value} />}
-        </ThemeContext.Consumer>
-    );
-}
-
-function Button(props) {
-    let buttonTheme = {};
-
-    switch (props.theme) {
-        case 'dark':
-            buttonTheme = { backgroundColor: 'black', color: 'white' };
-            break;
-        case 'light':
-            buttonTheme = { backgroundColor: 'white', color: 'black' };
-            break;
-        case 'green':
-            buttonTheme = { backgroundColor: 'green', color: 'yellow' };
-            break;
-        default:
-            buttonTheme = { backgroundColor: 'white', color: 'black' };
-    }
-
-    return (
-        <button
-            style={buttonTheme}
-        >
-            BUTTON
-        </button>
-    )
-}
-
-const ThemeContext = React.createContext('light');
+import './index.css';
 
 const App = () => {
-    const themes = ['dark', 'light', 'green', null];
-
     return (
         <div>
-            {themes.map((theme, index) => (
-                <ThemeContext.Provider key={index} value={theme}>
-                    <Toolbar />
-                </ThemeContext.Provider>
-            ))}
+            <h1>Animal Sound</h1>
+            <div id="animal">
+                <div class="cat">
+                    <span class="animal-name">Cat</span>
+                    <p class="animal-sound">"Meow~"</p>
+                </div>
+                <div class="dog">
+                    <span class="animal-name">Dog</span>
+                    <p class="animal-sound">"Woof!"</p>
+                </div>
+            </div>
+            <br />
+            <p class="dog"> == A different dog is passing by ==&gt;</p>
+            <br />
+            <div id="memo">
+                <h2>Cows make the sound "Moo".</h2>
+                <h3>Sheep make sound "Baa".</h3>
+                <h4>Birds make sound "Tweet".</h4>
+            </div>
         </div>
     );
 }
