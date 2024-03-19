c28514d150fbb95dba8cbe4b3f1324fb709ea237
spaceofin <ineemig@gmail.com>
2024-03-19 17:49:21 +0900

    [react-syntax-practice]update: practice counter app

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,51 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const handleButtonClick = (operation) => {
+        if (operation === 'decrease') {
+            setCount(count - 1);
+        } else if (operation === 'increase') {
+            setCount(count + 1);
+        }
+    };
+
+    return (
+        <div className="container">
+            <span className="span-title">Counter App</span>
+            <div className="control-buttons">
+                <button id="decrease-btn" onClick={() => handleButtonClick('decrease')}>&minus;</button>
+                <span>{count}</span>
+                <button id="increase-btn" onClick={() => handleButtonClick('increase')}>+</button>
+            </div>
+        </div >
+    );
+}
 
 const App = () => {
     return (
         <div>
-            <h1>Animal Sound</h1>
-            <div id="animal">
-                <div className="cat">
-                    <span className="animal-name">Cat</span>
-                    <p className="animal-sound">"Meow~"</p>
-                </div>
-                <div className="dog">
-                    <span className="animal-name">Dog</span>
-                    <p className="animal-sound">"Woof!"
-                    </p>
-                </div>
-            </div>
-            <br />
-            <p className="dog"> == A different dog is passing by ==&gt;</p>
-            <br />
-            <div id="memo">
-                <h2>Cows make the sound "Moo".</h2>
-                <h3>Sheep make sound "Baa".</h3>
-                <h4>Birds make sound "Tweet".</h4>
-            </div>
-            <br />
-            <div className="horizontal-line"></div>
-            <div id="state-selectors">
-                <button>Hover Me!</button>
-                <br />
-                <a href="./">Click Me!</a>
-                <br />
-                <input value="focus input" readOnly />
-                <br />
-                <select size='4'>
-                    <option value="spring">Spring</option>
-                    <option value="summer">Summer</option>
-                    <option value="autumn">Autumn</option>
-                    <option value="winter">Winter</option>
-                </select>
-            </div>
-            <div id="child-selectors">
-                <p>This is first p child.</p>
-                <p>This is nothing.</p>
-                <span>This is last span child.</span>
-            </div>
+            <Counter />
         </div >
     );
 }
