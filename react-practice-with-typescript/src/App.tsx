f5f6a00d2ef45d5a0c9a55dbd3f0984c2755975c
spaceofin <ineemig@gmail.com>
2024-03-21 13:27:17 +0900

    [react-practice-with-typescript]update: practice css-in-js emotion

--- react-practice-with-typescript/src/App.tsx
+++ react-practice-with-typescript/src/App.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+	text-align: center;
+`;
+
+const Header = styled.header`
+	background-color: #282c34;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	font-size: calc(10px + 2vmin);
+	color: white;
+`;
+
+const AppLogo = styled.img`
+	height: 40vmin;
+	pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+      animation: App-logo-spin infinite 20s linear;
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    // <div className="App">
+    <Container>
+      {/* <header className="App-header"> */}
+      <Header>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <AppLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -18,8 +55,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </Header>
+      {/* </header> */}
+    </Container>
+    // </div>
   );
 }
 
