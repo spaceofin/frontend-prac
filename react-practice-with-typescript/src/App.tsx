97382e732ef8ada8e01b9b6b4b141295a498ea2c
spaceofin <ineemig@gmail.com>
2024-03-21 18:21:32 +0900

    [react-practice-with-typescript]update: practice prettier

--- react-practice-with-typescript/src/App.tsx
+++ react-practice-with-typescript/src/App.tsx
@@ -4,18 +4,18 @@ import styled from '@emotion/styled';
 import { keyframes } from '@emotion/react';
 
 const Container = styled.div`
-	text-align: center;
+  text-align: center;
 `;
 
 const Header = styled.header`
-	background-color: #282c34;
-	min-height: 100vh;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	font-size: calc(10px + 2vmin);
-	color: white;
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
 `;
 
 const AppLogoSpin = keyframes`
@@ -28,18 +28,17 @@ const AppLogoSpin = keyframes`
 `;
 
 const AppLogo = styled.img`
-	height: 40vmin;
-	pointer-events: none;
+  height: 40vmin;
+  pointer-events: none;
 
   @media (prefers-reduced-motion: no-preference) {
-      animation: ${AppLogoSpin} infinite 20s linear;
+    animation: ${AppLogoSpin} infinite 20s linear;
   }
 `;
 
 const AppLink = styled.a`
-	color: #61dafb;
+  color: #61dafb;
 `;
-
 
 function App() {
   return (
