d8962494a7acbab5ab909d53e5e589acbdb1b79b
spaceofin <ineemig@gmail.com>
2024-03-22 17:15:37 +0900

    [react-practice-with-typescript]update: practice counter app

--- react-practice-with-typescript/src/App.tsx
+++ react-practice-with-typescript/src/App.tsx
@@ -1,60 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import styled from '@emotion/styled';
-import { keyframes } from '@emotion/react';
 
 const Container = styled.div`
-  text-align: center;
-`;
-
-const Header = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 350px;
+  height: 200px;
+  border: 1px solid black;
+  border-radius: 5px;
+  margin: 50px;
+`;
+
+const Title = styled.div`
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: bold;
+  font-size: 30px;
+`;
+
+const Contents = styled.div`
+  display: flex;
   align-items: center;
   justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
 `;
 
-const AppLogoSpin = keyframes`
-	from {
-		transform: rotate(0deg);
-	}
-	to {
-		transform: rotate(360deg);
-	}
+const Label = styled.span`
+  font-size: 18px;
+  margin: 15px;
 `;
 
-const AppLogo = styled.img`
-  height: 40vmin;
-  pointer-events: none;
+const Button = styled.button`
+  margin: 0px 10px;
+  width: 40px;
+  height: 30px;
+  font-weight: bold;
+  border-radius: 5px;
+  cursor: pointer;
 
-  @media (prefers-reduced-motion: no-preference) {
-    animation: ${AppLogoSpin} infinite 20s linear;
+  &:hover {
+    background-color: #0091EA;
+    opacity: 0.9;
+    border-radius: 5px;
+  }
+
+  &:active {
+    box-shadow: 2px 2px 2px #FF4081;
   }
 `;
 
-const AppLink = styled.a`
-  color: #61dafb;
-`;
-
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleButtonClick = (operation: string) => {
+    if (operation === 'decrease') {
+      setCounter(counter - 1);
+    } else if (operation === 'increase') {
+      setCounter(counter + 1);
+    }
+  };
+
   return (
     <Container>
-      <Header>
-        <AppLogo src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <AppLink
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </AppLink>
-      </Header>
+      <Title>Counter App</Title>
+      <Contents>
+        <Button onClick={() => handleButtonClick('decrease')}>-</Button>
+        <Label>{counter}</Label>
+        <Button onClick={() => handleButtonClick('increase')}>+</Button>
+      </Contents>
     </Container>
   );
 }
