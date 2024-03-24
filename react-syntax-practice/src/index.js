469ead9328afe1508aeca03460a0b1c1b8e78cf7
spaceofin <ineemig@gmail.com>
2024-03-24 17:49:26 +0900

    [react-syntax-practice]update: practice to-do list app

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,84 +1,54 @@
-// import React, { useState } from "react";
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-// const Counter = () => {
-//     const [count, setCount] = useState(0);
 
-//     const handleButtonClick = (operation) => {
-//         if (operation === 'decrease') {
-//             setCount(count - 1);
-//         } else if (operation === 'increase') {
-//             setCount(count + 1);
-//         }
-//     };
-
-//     return (
-//         <div className="container">
-//             <span className="span-title">Counter App</span>
-//             <div className="control-buttons">
-//                 <button id="decrease-btn" onClick={() => handleButtonClick('decrease')}>&minus;</button>
-//                 <span>{count}</span>
-//                 <button id="increase-btn" onClick={() => handleButtonClick('increase')}>+</button>
-//             </div>
-//         </div >
-//     );
-// }
-
-// const App = () => {
-//     return (
-//         <div>
-//             <Counter />
-//         </div >
-//     );
-// }
-
-class Counter extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0,
-        };
-    }
-
-    handleButtonClick(operation) {
-        if (operation === 'decrease') {
-            // this.setState({ count: this.state.count - 1, });
-            this.setState(prevState => ({
-                count: prevState.count - 1
-            }))
-        } else if (operation === 'increase') {
-            // this.setState({ count: this.state.count + 1, });
-            this.setState(prevState => ({
-                count: prevState.count + 1
-            }))
-        }
-    };
-
-    render() {
-        return (
-            <div className="container">
-                <span className="span-title">Counter App</span>
-                <div className="control-buttons">
-                    <button id="decrease-btn" onClick={() => this.handleButtonClick('decrease')}>&minus;</button>
-                    <span>{this.state.count}</span>
-                    <button id="increase-btn" onClick={() => this.handleButtonClick('increase')}>+</button>
-                </div>
-            </div >
-        );
-    }
+const Card = (props) => {
+    return (
+        <div>
+            <span className="todo-item">{props.item}</span>
+        </div>
+    )
 }
 
-class App extends React.Component {
-    render() {
-        return (
+const Container = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [toDoList, setToDoList] = useState([]);
+
+    const handleClick = () => {
+        setToDoList([...toDoList, <Card item={inputValue} />]);
+        setInputValue('');
+    }
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    return (
+        <div className="container">
+            <h1 className="title">To Do List</h1>
             <div>
-                <Counter />
-            </div >
-        );
-    }
+                <input value={inputValue} onChange={handleChange} />
+                <button className="btn-addtodolist" onClick={handleClick}>Add List</button>
+            </div>
+            <div className="cardzone">
+                {toDoList.map((card, index) => (
+                    <div key={index}>{card}</div>
+                ))}
+            </div>
+
+        </div>
+    );
 }
+
+const App = () => {
+    return (
+        <div>
+            <Container />
+        </div >
+    );
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />)
