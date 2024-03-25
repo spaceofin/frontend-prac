178bf582aff2f598bbb5cea990cdde3ea964e704
spaceofin <ineemig@gmail.com>
2024-03-25 13:08:28 +0900

    [react-syntax-practice]update: practice to-do list app - feat del item using uuid

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
 const Card = (props) => {
     return (
-        <div>
+        <div className="item">
             <span className="todo-item">{props.item}</span>
+            <button className="btn-delete-item" onClick={props.onClick}>Del</button>
         </div>
     )
 }
@@ -15,9 +17,15 @@ const Container = () => {
     const [inputValue, setInputValue] = useState('');
     const [toDoList, setToDoList] = useState([]);
 
-    const handleClick = () => {
-        setToDoList([...toDoList, <Card item={inputValue} />]);
+    const handleAddListClick = () => {
+        const newItem = { id: uuidv4(), item: inputValue };
+        setToDoList([...toDoList, newItem]);
         setInputValue('');
+    }
+
+    const handleDelClick = (id) => {
+        const updatedList = toDoList.filter(item => item.id !== id);
+        setToDoList(updatedList);
     }
 
     const handleChange = (event) => {
@@ -29,11 +37,11 @@ const Container = () => {
             <h1 className="title">To Do List</h1>
             <div>
                 <input value={inputValue} onChange={handleChange} />
-                <button className="btn-addtodolist" onClick={handleClick}>Add List</button>
+                <button className="btn-add-item" onClick={handleAddListClick}>Add List</button>
             </div>
             <div className="cardzone">
                 {toDoList.map((card, index) => (
-                    <div key={index}>{card}</div>
+                    <div key={index}><Card item={card.item} onClick={() => handleDelClick(card.id)} /></div>
                 ))}
             </div>
 
