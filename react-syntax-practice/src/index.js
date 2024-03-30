a4484b08a6cbfb69579262fc650f8fa6ae798f84
spaceofin <ineemig@gmail.com>
2024-03-30 23:02:03 +0900

    [react-syntax-practice]update: practice react redux tutorial

--- react-syntax-practice/src/index.js
+++ react-syntax-practice/src/index.js
@@ -1,72 +1,15 @@
-import React, { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import store from './store'
+import { Provider } from 'react-redux'
 
+// As of React 18
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const Card = (props) => {
-    return (
-        <div className="item">
-            <span className="todo-item">{props.item}</span>
-            <button className="btn-delete-item" onClick={props.onClick}>Del</button>
-        </div>
-    )
-}
-
-const Container = () => {
-    const [inputValue, setInputValue] = useState('');
-    const [addListVisible, setAddListVisible] = useState(false);
-    const [buttonName, setButtonName] = useState('Add List');
-    const [toDoList, setToDoList] = useState([]);
-
-    const handleAddListClick = () => {
-        setButtonName(buttonName === 'Add List' ? 'Close' : 'Add List');
-        setAddListVisible(prevState => !prevState);
-    }
-
-    const handleAddClick = () => {
-        const newItem = { id: uuidv4(), item: inputValue };
-        setToDoList([...toDoList, newItem]);
-        setInputValue('');
-        handleAddListClick();
-    }
-
-    const handleDelClick = (id) => {
-        const updatedList = toDoList.filter(item => item.id !== id);
-        setToDoList(updatedList);
-    }
-
-    const handleChange = (event) => {
-        setInputValue(event.target.value);
-    }
-
-    return (
-        <div className={addListVisible ? "add-list-container" : "container"}>
-            <button className="btn-add-list" onClick={handleAddListClick}>{buttonName}</button>
-            <h1 className="title">{addListVisible ? 'Add List' : 'To Do List'}</h1>
-            {addListVisible ? (
-                <div>
-                    <input value={inputValue} onChange={handleChange} />
-                    <button className="btn-add-item" onClick={handleAddClick}>Add</button>
-                </div>
-            ) : <div className="cardzone">
-                {toDoList.map((card, index) => (
-                    <div key={index}><Card item={card.item} onClick={() => handleDelClick(card.id)} /></div>
-                ))}
-            </div>}
-        </div>
-
-    );
-}
-
-const App = () => {
-    return (
-        <div>
-            <Container />
-        </div >
-    );
-}
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />)
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+)
