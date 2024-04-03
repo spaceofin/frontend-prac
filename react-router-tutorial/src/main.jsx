aed8c07b7a6ef5cabe128d99b112bb0de5ceb488
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-04-03 23:51:10 +0900

    [react-router-tutorial]update: implement delete records

--- react-router-tutorial/src/main.jsx
+++ react-router-tutorial/src/main.jsx
@@ -13,6 +13,7 @@ import Contact, {
 import EditContact, {
   action as editAction,
 } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
 
 const router = createBrowserRouter([
   {
@@ -32,6 +33,10 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       },
     ],
   },
