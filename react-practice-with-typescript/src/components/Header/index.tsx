20288e950493b5ed8d7e4b023951faf3c8638e6c
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-04-12 00:07:35 +0900

    [react-practice-with-typescript]update: practice fetch api - update header style

--- react-practice-with-typescript/src/components/Header/index.tsx
+++ react-practice-with-typescript/src/components/Header/index.tsx
@@ -3,16 +3,17 @@ import styled from '@emotion/styled';
 const Container = styled.div`
     display: flex;
     justify-content: center;
-	background-color: #ffffff;
-	padding: 25px;
+    background-image: linear-gradient(to bottom, #11DEBD, #1b89ba);
+	padding: 50px;
 	width: calc(100% - 40px);
 	margin-bottom: 20px;
-    margin-top: 20px;
 `;
 
 const Title = styled.div`
-	font-size: 1.5rem;
+	font-size: 1.7rem;
 	font-weight: normal;
+    font-family: cursive;
+    color: #ffffff;
 `;
 
 export const Header = () => {
