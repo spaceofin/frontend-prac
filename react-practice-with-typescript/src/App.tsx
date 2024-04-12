ac0e77333a4a7383568a2f3ebd99e6ef6c520af8
spaceofin <ineemig@gmail.com>
2024-04-12 23:12:57 +0900

    [react-practice-with-typescript]update: practice fetch api - update post component using props

--- react-practice-with-typescript/src/App.tsx
+++ react-practice-with-typescript/src/App.tsx
@@ -1,22 +1,21 @@
-import styled from '@emotion/styled';
-import { Post } from 'components/Post';
-import { Header } from 'components/Header';
+import styled from "@emotion/styled";
+import { Post } from "components/Post";
+import { Header } from "components/Header";
 
 const Container = styled.div`
-	height: 100vh;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	background-color: #1b89ba;
-	overflow: scroll;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #1b89ba;
+  overflow: scroll;
 `;
-
 
 function App() {
   return (
     <Container>
       <Header />
-      <Post />
+      <Post title="Title 1" body="Body 1" />
     </Container>
   );
 }
--- react-practice-with-typescript/src/components/Post/index.tsx
+++ react-practice-with-typescript/src/components/Post/index.tsx
@@ -1,30 +1,35 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
 const Container = styled.div`
-width: 80vw;
-	background-color: #ffffff;
-	padding: 20px;
-	margin: 20px;
-	border-radius: 10px;
-	box-shadow: 7px 7px 2px #14678C;
+  width: 80vw;
+  background-color: #ffffff;
+  padding: 20px;
+  margin: 20px;
+  border-radius: 10px;
+  box-shadow: 7px 7px 2px #14678c;
 `;
 
 const Title = styled.div`
-	font-weight: bold;
-	margin-bottom: 10px;
+  font-weight: bold;
+  margin-bottom: 10px;
 `;
 
 const Body = styled.div`
-	white-space: nowrap;
-	text-overflow: ellipsis;
-	overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
 `;
 
-export const Post = () => {
-    return (
-        <Container>
-            <Title>Title 1</Title>
-            <Body>Body 1</Body>
-        </Container>
-    );
+interface Props {
+  readonly title: string;
+  readonly body: string;
+}
+
+export const Post = ({ title, body }: Props) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Body>{body}</Body>
+    </Container>
+  );
 };
