e1649574b14b3d785acdff002c4eec22a4a6c5f4
spaceofin <ineemig@gmail.com>
2024-04-13 16:48:00 +0900

    [react-practice-with-typescript]update: practice fetch api - display dummy data

--- react-practice-with-typescript/src/App.tsx
+++ react-practice-with-typescript/src/App.tsx
@@ -11,11 +11,41 @@ const Container = styled.div`
   overflow: scroll;
 `;
 
+interface Post {
+  readonly id: number;
+  readonly userId: number;
+  readonly title: string;
+  readonly body: string;
+}
+
 function App() {
+  const posts: ReadonlyArray<Post> = [
+    {
+      id: 1,
+      userId: 1,
+      title: "Title1",
+      body: "Body1",
+    },
+    {
+      id: 2,
+      userId: 1,
+      title: "Title2",
+      body: "Body2",
+    },
+    {
+      id: 3,
+      userId: 2,
+      title: "Title3",
+      body: "Body3",
+    },
+  ];
+
   return (
     <Container>
       <Header />
-      <Post title="Title 1" body="Body 1" />
+      {posts.map((post) => (
+        <Post key={post.id} title={post.title} body={post.body} />
+      ))}
     </Container>
   );
 }
