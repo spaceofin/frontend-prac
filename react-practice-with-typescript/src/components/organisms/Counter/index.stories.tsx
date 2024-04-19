69279d5214b12b7bc5da1ba2702c174d7ef189cc
spaceofin <ineemig@gmail.com>
2024-04-19 16:39:19 +0900

    [react-practice-with-typescript]update: add counter component

---
+++ react-practice-with-typescript/src/components/organisms/Counter/index.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Counter } from '.';
+
+const meta: Meta<typeof Counter> = {
+  title: 'Organisms/Counter',
+  component: Counter,
+};
+
+export default meta;
+
+type Counter = StoryObj<typeof Counter>;
+
+export const Default: Counter = {
+  args: {
+    value: 0,
+  },
+};
---
+++ react-practice-with-typescript/src/components/organisms/Counter/index.tsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+import { Button } from 'components/atoms/Button';
+import { Count } from 'components/atoms/Count';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Counter = () => {
+  return (
+    <Container>
+      <Button label="-" />
+      <Count value={0} />
+      <Button label="+" />
+    </Container>
+  );
+};
