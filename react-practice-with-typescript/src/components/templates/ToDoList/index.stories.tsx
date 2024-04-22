3e218a1f8f8ec7d9a756730ab7382fd452da2477
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-04-22 15:28:27 +0900

    [react-practice-with-typescript]update: add todolist component

---
+++ react-practice-with-typescript/src/components/templates/ToDoList/index.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ToDoList } from '.';
+
+const meta: Meta<typeof ToDoList> = {
+  title: 'Templates/ToDoList',
+  component: ToDoList,
+};
+export default meta;
+
+type ToDoList = StoryObj<typeof ToDoList>;
+
+export const Default: ToDoList = {
+  args: {},
+};
