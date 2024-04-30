6022561a674c31768119b530a82d4a23256867d2
spaceofin <ineemig@gmail.com>
2024-04-30 14:14:33 +0900

    [react-practice-with-typescript]feat: update App component - add UserProfile component

--- react-practice-with-typescript/src/App.tsx
+++ react-practice-with-typescript/src/App.tsx
@@ -1,5 +1,49 @@
-function App() {
-  return <div className="App">Hello, React</div>;
-}
+import styled from "styled-components";
 
-export default App;
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 20px;
+  width: 500px;
+`;
+
+const ImageWrapper = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: lightGrey;
+`;
+
+const ProfileContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: 20px;
+  background-color: Grey;
+`;
+
+const LineText = styled.span`
+  font-size: 20px;
+  background-color: orange;
+`;
+
+const UserProfile = () => {
+  return (
+    <Container>
+      <ImageWrapper />
+      <ProfileContainer>
+        <LineText>NAME</LineText>
+        <LineText>JOB</LineText>
+        <LineText>DATE</LineText>
+      </ProfileContainer>
+    </Container>
+  );
+};
+
+export default function App() {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  );
+}
