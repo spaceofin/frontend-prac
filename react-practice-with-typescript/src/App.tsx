9a1b5d7f211cd1b986b2cfaa1147b22e8c5142f9
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-05-01 23:53:02 +0900

    [react-practice-with-typescript]refactor: separate UserProfile component from App.tsx into its own file

--- react-practice-with-typescript/src/App.tsx
+++ react-practice-with-typescript/src/App.tsx
@@ -1,51 +1,12 @@
 import styled from "styled-components";
+import { UserProfile } from "components/UserProfile";
 
-import * as Data from "data";
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin: 20px;
-  padding: 15px;
-  width: 500px;
-  background-color: #CCED71;
-  border-radius: 10px;
-`;
-
-const ImageWrapper = styled.img`
-  border-radius: 10px;
-`;
-
-const ProfileContainer = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  padding: 20px;
-`;
-
-const LineText = styled.span`
-  font-family: cursive;
-  font-size: 20px;
-`;
-
-const UserProfile = () => {
-  return (
-    <Container>
-      <ImageWrapper src={Data.picsumUrl()} />
-      <ProfileContainer>
-        <LineText>{Data.randomName()}</LineText>
-        <LineText>{Data.randomProfession()}</LineText>
-        <LineText>{Data.randomDayMonthYear()}</LineText>
-      </ProfileContainer>
-    </Container>
-  );
-};
+const Container = styled.div``;
 
 export default function App() {
   return (
-    <div>
+    <Container>
       <UserProfile />
-    </div>
+    </Container>
   );
 }
---
+++ react-practice-with-typescript/src/components/UserProfile/index.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import * as Data from "data";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 20px;
+  padding: 15px;
+  width: 500px;
+  background-color: #cced71;
+  border-radius: 10px;
+`;
+
+const ImageWrapper = styled.img`
+  border-radius: 10px;
+`;
+
+const ProfileContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const LineText = styled.span`
+  font-family: cursive;
+  font-size: 20px;
+`;
+
+export const UserProfile = () => {
+  return (
+    <Container>
+      <ImageWrapper src={Data.picsumUrl()} />
+      <ProfileContainer>
+        <LineText>{Data.randomName()}</LineText>
+        <LineText>{Data.randomProfession()}</LineText>
+        <LineText>{Data.randomDayMonthYear()}</LineText>
+      </ProfileContainer>
+    </Container>
+  );
+};
