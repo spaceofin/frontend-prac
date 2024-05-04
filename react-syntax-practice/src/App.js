5c1143517ce0805c7e49c99f37d7cfba212c9359
spaceofin <ineemig@gmail.com>
2024-05-04 22:22:13 +0900

    [react-syntax-practice]style: style App component

--- react-syntax-practice/src/App.js
+++ react-syntax-practice/src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div>Hello, React JS!</div>;
-}
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #398aff;
+  padding: 50px;
+  color: white;
+  font-size: 20px;
+`;
+
+const App = () => {
+  return <Container>Hello, React JS!</Container>;
+};
 
 export default App;
