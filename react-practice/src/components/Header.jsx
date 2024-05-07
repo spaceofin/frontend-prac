b6a2d6b3b7394648de67579311dc43bb2833ed4c
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-05-08 01:42:59 +0900

    [react-practice]update: update Header component

--- react-practice/src/components/Header.jsx
+++ react-practice/src/components/Header.jsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <header className="d-flex flex-wrap justify-content-center py-3 pt-3 pb-3 ps-3 pe-5 fs-5 bg-secondary-subtle">
       <a
         href="/"
         className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
@@ -8,12 +8,19 @@ export const Header = () => {
         <svg className="bi me-2" width="40" height="32">
           <use href="#bootstrap"></use>
         </svg>
-        <span className="fs-4">MYCN</span>
+
+        <span className="material-icons">explore</span>
+        <span className="fs-3">MYCN</span>
       </a>
 
       <ul className="nav nav-pills">
         <li className="nav-item">
-          <a href="/" className="nav-link active" aria-current="page">
+          <a
+            href="/"
+            className="nav-link active"
+            aria-current="page"
+            style={{ backgroundColor: "rgb(228, 29, 63)" }}
+          >
             About
           </a>
         </li>
