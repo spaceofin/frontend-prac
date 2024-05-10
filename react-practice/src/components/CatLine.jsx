b31c1b4efe6816c6254271d59f990862e6b38eb4
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-05-10 23:56:05 +0900

    [react-practice]update: update CatLine component to rotate cat icons

--- react-practice/src/components/CatLine.jsx
+++ react-practice/src/components/CatLine.jsx
@@ -1,13 +1,41 @@
+import styled, { keyframes } from "styled-components";
 import { ReactComponent as Cat1 } from "../assets/images/cat1.svg";
 import { ReactComponent as Cat2 } from "../assets/images/cat2.svg";
 import { ReactComponent as Cat3 } from "../assets/images/cat3.svg";
 
+const roll = keyframes`
+  0% {
+    transform: translateX(0) rotate(0deg);
+  }
+  25% {
+    transform: translateX(25vw) rotate(360deg);
+  }
+  50% {
+    transform: translateX(50vw) rotate(720deg);
+  }
+  100% {
+    transform: translateX(100vw) rotate(1080deg);
+  }
+`;
+
+const StyledCat1 = styled(Cat1)`
+  animation: ${roll} 20s linear infinite;
+`;
+
+const StyledCat2 = styled(Cat2)`
+  animation: ${roll} 20s linear infinite;
+`;
+
+const StyledCat3 = styled(Cat3)`
+  animation: ${roll} 20s linear infinite;
+`;
+
 export const CatLine = () => {
   return (
     <div>
-      <Cat1 />
-      <Cat2 />
-      <Cat3 />
+      <StyledCat1 />
+      <StyledCat2 />
+      <StyledCat3 />
     </div>
   );
 };
