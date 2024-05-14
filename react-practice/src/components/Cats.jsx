53062ac9ea74371de32661f17aeb812f9ca22885
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-05-14 22:35:44 +0900

    [react-practice]feat: fetch cat imageUrl data from the cat api in Cats component

--- react-practice/src/components/Cats.jsx
+++ react-practice/src/components/Cats.jsx
@@ -1,6 +1,34 @@
 import { Header } from "./Header";
 
 export const Cats = () => {
+  const url = process.env.REACT_APP_CAT_API_URL;
+  const apiKey = process.env.REACT_APP_CAT_API_KEY;
+
+  // console.log(url);
+  // console.log(apiKey);
+  let imgUrls = [];
+
+  fetch(url, {
+    headers: {
+      "x-api-key": apiKey,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      data = data.filter((img) => img?.url != null);
+      // console.log(data);
+      imgUrls = data;
+
+      for (let i = 0; i < imgUrls.length; i++) {
+        console.log(imgUrls[i].url);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
   return (
     <div>
       <Header />
