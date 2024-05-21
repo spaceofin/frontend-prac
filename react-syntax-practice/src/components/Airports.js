2a2a92857fc761aff7944ad0f8aba9e4256158c1
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-05-22 00:13:11 +0900

    [react-syntax-practice]feat: display airport code when clicked on

--- react-syntax-practice/src/components/Airports.js
+++ react-syntax-practice/src/components/Airports.js
@@ -1,5 +1,6 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import airportsList from "../data/airports-list.json";
+import { useState } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -14,15 +15,50 @@ const Container = styled.div`
 
 const AirportWrapper = styled.div`
   margin: 2px 0px;
+  padding: 0px 10px;
+  transition: color 0.5s;
+  width: 350px;
+  display: flex;
+  justify-content: space-between;
+
+  // background-color: ${({ clicked }) => (clicked ? "white" : null)};
+
+  ${({ clicked }) =>
+    clicked &&
+    css`
+      color: slateblue;
+      background-color: white;
+      border-radius: 5px;
+      font-weight: 600;
+    `}
+
+  &:hover {
+    color: slateblue;
+    cursor: pointer;
+  }
 `;
 
 export const Airports = () => {
   console.log("Airports component");
   // console.log(airportsList);
+
+  const [selectedAirport, setSelectedAirport] = useState(null);
+
+  const handleClick = (airport) => {
+    setSelectedAirport(selectedAirport === airport.id ? null : airport.id);
+  };
+
   return (
     <Container>
       {airportsList.map((airport) => (
-        <AirportWrapper key={airport.id}>{airport.name}</AirportWrapper>
+        <AirportWrapper
+          key={airport.id}
+          onClick={() => handleClick(airport)}
+          clicked={selectedAirport === airport.id}
+        >
+          <span>{airport.name}</span>
+          {selectedAirport === airport.id && <span>CODE: {airport.code}</span>}
+        </AirportWrapper>
       ))}
     </Container>
   );
