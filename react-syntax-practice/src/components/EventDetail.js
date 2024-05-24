1066389904031ff07c832f1a2608d8c129d92059
spaceofin <ineemig@gmail.com>
2024-05-24 16:41:55 +0900

    [react-syntax-practice]style: style EventDetail component

--- react-syntax-practice/src/components/EventDetail.js
+++ react-syntax-practice/src/components/EventDetail.js
@@ -1,3 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #830fff;
+  padding: 50px;
+  color: white;
+  font-size: 20px;
+`;
+
 export const EventDetail = () => {
-  return <div>This is Event Detail Page.</div>;
+  return <Container>This is Event Detail Page.</Container>;
 };
