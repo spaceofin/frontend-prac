12d8b3408c2ed345433cab571549ab382ecd9130
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-05-25 23:11:28 +0900

    [react-practice-with-typescript]feat: implement Counter using useReducer

--- react-practice-with-typescript/src/components/Counter/index.tsx
+++ react-practice-with-typescript/src/components/Counter/index.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useReducer } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -13,6 +14,36 @@ const Container = styled.div`
   border-radius: 10px;
 `;
 
+type State = {
+  count: number;
+};
+type Action = { type: "increment" } | { type: "decrement" } | { type: "reset" };
+type Reducer = (state: State, action: Action) => State;
+
+const initialState: State = { count: 0 };
+
+const reducer: Reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    case "reset":
+      return initialState;
+    default:
+      throw new Error();
+  }
+};
+
 export const Counter = () => {
-  return <Container>Counter</Container>;
+  const [state, dispatch] = useReducer<Reducer>(reducer, initialState);
+
+  return (
+    <Container>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+    </Container>
+  );
 };
