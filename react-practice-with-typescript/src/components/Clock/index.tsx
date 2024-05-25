10e220221837f11cb39b2c96f7b4fb292f7ff6f4
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-05-25 23:26:18 +0900

    [react-practice-with-typescript]style: style Clock component

--- react-practice-with-typescript/src/components/Clock/index.tsx
+++ react-practice-with-typescript/src/components/Clock/index.tsx
@@ -2,26 +2,34 @@ import styled from "styled-components";
 import type { FC } from "react";
 
 const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 500px;
+  height: 50px;
   margin: 20px;
   margin-bottom: 5px;
   padding: 15px;
-  background: linear-gradient(to right, #18e9ff, #19ffb4, #17ff7d);
+  // background: linear-gradient(to right, #18e9ff, #19ffb4, #17ff7d);
+  background-color: #15f7a9;
   border-radius: 10px;
+  border: 5px solid #1cc9a1;
 `;
 
 const DateWrapper = styled.div`
   display: flex;
   justify-content: center;
-  font-family: Verdana, Arial, Helvetica, sans-serif;
-  font-size: 25px;
+  // font-family: Verdana, Arial, Helvetica, sans-serif;
+  font-size: 27px;
+  margin: 0px 10px;
 `;
 
 const TimeWrapper = styled.div`
   display: flex;
   justify-content: center;
-  font-family: Verdana, Arial, Helvetica, sans-serif;
-  font-size: 20px;
+  // font-family: Verdana, Arial, Helvetica, sans-serif;
+  font-size: 27px;
+  font-weight: ;
 `;
 
 export type ClockProps = {
