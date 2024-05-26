989bd4e734d68c495590762fa4203dba6b4d1592
spaceofin <ineemig@gmail.com>
2024-05-26 17:17:28 +0900

    [react-practice-with-typescript]feat: add MousePosition component

--- react-practice-with-typescript/src/App.tsx
+++ react-practice-with-typescript/src/App.tsx
@@ -3,6 +3,7 @@ import { useClock } from "hooks";
 
 import { Clock } from "components/Clock";
 import { Counter } from "components/Counter";
+import { MousePosition } from "components/MousePosition";
 
 const Container = styled.div`
   margin: 5px;
@@ -15,6 +16,7 @@ export default function App() {
     <Container>
       <Clock date={now} />
       <Counter />
+      <MousePosition />
     </Container>
   );
 }
---
+++ react-practice-with-typescript/src/components/MousePosition/index.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  height: 200px;
+  margin: 20px;
+  margin-bottom: 5px;
+  padding: 15px;
+  background-color: #eda7c9;
+  border-radius: 10px;
+  border: 5px solid #db7498;
+  font-size: 25px;
+  font-weight: bold;
+`;
+
+export const MousePosition = () => {
+  return <Container>MousePosition</Container>;
+};
