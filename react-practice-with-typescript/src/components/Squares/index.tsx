b850222293269c68d10a7286184c949eee4e96d5
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-05-26 23:18:02 +0900

    [react-practice-with-typescript]feat: update Squares component to practice useReducer

--- react-practice-with-typescript/src/components/Squares/index.tsx
+++ react-practice-with-typescript/src/components/Squares/index.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+import { useReducer } from "react";
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   width: 500px;
   height: 500px;
   margin: 20px;
@@ -14,6 +16,54 @@ const Container = styled.div`
   font-weight: bold;
 `;
 
+const StyledButton = styled.button`
+  width: 70px;
+  height: 50px;
+  margin: 20px;
+`;
+
+type SquareType = { id: number; color: string };
+
+const SQUARES_ACTION = {
+  ADD: "addSquare" as const,
+};
+
+const SquaresActionCreator = {
+  addSquare: (id: number, color: string) => ({
+    type: SQUARES_ACTION.ADD,
+    payload: { id: id, color: color },
+  }),
+};
+
+type SquaresActionType = ReturnType<typeof SquaresActionCreator.addSquare>;
+
+const SquaresReducer = (
+  state: Array<SquareType>,
+  action: SquaresActionType
+) => {
+  switch (action.type) {
+    case SQUARES_ACTION.ADD:
+      return [...state, { id: action.payload.id, color: action.payload.color }];
+    default:
+      return state;
+  }
+};
+
+const initialSquares: Array<SquareType> = [
+  { id: 1, color: "red" },
+  { id: 2, color: "blue" },
+];
+
 export const Squares = () => {
-  return <Container>Squares</Container>;
+  const [state, dispatch] = useReducer(SquaresReducer, initialSquares);
+  const addSquare = () => {
+    dispatch(SquaresActionCreator.addSquare(3, "orange"));
+  };
+  console.log("state", state);
+  return (
+    <Container>
+      Squares
+      <StyledButton onClick={() => addSquare()}>dispatch</StyledButton>
+    </Container>
+  );
 };
