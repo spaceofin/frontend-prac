ff00f895e405d1070072f8c6115533232efe65f7
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-05-31 00:08:26 +0900

    [react-practice]feat: add check-circle icon to photo

--- react-practice/src/components/Photo.jsx
+++ react-practice/src/components/Photo.jsx
@@ -1,13 +1,29 @@
 import styled from "styled-components";
+import { ReactComponent as CheckCircle } from "../assets/icons/check-circle.svg";
+
+const Container = styled.div`
+  position: relative;
+`;
 
 const StyledImg = styled.img`
   border-radius: 5px;
   margin: 5px;
 `;
 
+const StyledCheckCircle = styled(CheckCircle)`
+  position: absolute;
+  top: 15px;
+  right: 20px;
+`;
+
 export const Photo = ({ index }) => {
   const baseUrl = "https://picsum.photos/200/200";
   const queryString = `?random=${index}`;
 
-  return <StyledImg src={baseUrl + queryString} alt="random image" />;
+  return (
+    <Container>
+      <StyledCheckCircle />
+      <StyledImg src={baseUrl + queryString} alt="random image" />
+    </Container>
+  );
 };
