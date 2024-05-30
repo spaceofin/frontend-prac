bcfcd80c3b4f46c8c8a3c82eeebce38a57526129
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-05-30 16:40:59 +0900

    [frontend-prac]chore: clean up repository

--- do-it-example-react/src/stories/ReduxAppStory.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import ReduxApp01 from '../07/ReduxApp01';
-
-storiesOf('ReduxApp', module).add('기본 스토어 설정', () => <ReduxApp01 />);
