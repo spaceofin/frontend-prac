39727e60bbab801ec318077ad1f3308fc269ebcd
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-06-26 00:32:01 +0900

    [react-syntax-practice]feat: update Weather component to display city name and weather based on latitude and longitude

--- react-syntax-practice/src/components/Weather.js
+++ react-syntax-practice/src/components/Weather.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { useState } from "react";
+import axios from "axios";
 
 const Container = styled.div`
   // display: flex;
@@ -9,12 +11,65 @@ const Container = styled.div`
   background-color: #f76854;
 `;
 
+const Title = styled.h1`
+  font-size: 30px;
+  font-weight: normal;
+`;
+
+const InputGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+`;
+
 export const Weather = () => {
+  const [weather, setWeather] = useState(null);
+  const [lat, setLat] = useState("");
+  const [lon, setLon] = useState("");
+
   const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+  const getWeather = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+      );
+      console.log("response:", response);
+      console.log("city:", response.data.name);
+      setWeather(response.data);
+    } catch (err) {
+      console.log("error:", err);
+      setWeather(null);
+    }
+  };
 
   return (
     <Container>
-      <div>Weather App</div>
+      <Title>Weather App</Title>
+      <InputGroup>
+        <input
+          type="text"
+          value={lat}
+          onChange={(e) => setLat(e.target.value)}
+          placeholder="Enter lat"
+        />
+        <input
+          type="text"
+          value={lon}
+          onChange={(e) => setLon(e.target.value)}
+          placeholder="Enter lon"
+        />
+        <button onClick={getWeather}>Get Weather</button>
+        {weather && (
+          <div>
+            <p>
+              CITY: {weather.name}
+              <br />
+              WEATHER: {weather.weather[0].description}
+            </p>
+          </div>
+        )}
+      </InputGroup>
     </Container>
   );
 };
