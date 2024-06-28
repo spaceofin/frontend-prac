1e0286860e72f64ca0bcc53cc2c4c44572adea7e
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-06-28 20:02:03 +0900

    [react-practice-with-typescript]feat: enable icons to be draggable

--- react-practice-with-typescript/src/components/Icons/index.tsx
+++ react-practice-with-typescript/src/components/Icons/index.tsx
@@ -4,8 +4,10 @@ import { ReactComponent as WaterDrop } from "assets/icons/water-drop.svg";
 import { ReactComponent as Diamond } from "assets/icons/diamond.svg";
 import { ReactComponent as StrokeFull } from "assets/icons/stroke-full.svg";
 import { ReactComponent as Robot } from "assets/icons/robot.svg";
+import { useState, useRef, MouseEvent } from "react";
 
 const Container = styled.div`
+  position: relative;
   display: grid;
   grid-template-columns: repeat(5, 90px);
   justify-content: center;
@@ -22,14 +24,54 @@ const Container = styled.div`
   font-weight: bold;
 `;
 
+const DraggableIcon = styled.div<{ x: number; y: number }>`
+  position: absolute;
+  left: ${(props) => props.x}px;
+  top: ${(props) => props.y}px;
+  cursor: pointer;
+`;
+
 export const Icons = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [dragging, setDragging] = useState(false);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+  const iconRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
+    setDragging(true);
+    setOffset({
+      x: e.clientX - position.x,
+      y: e.clientY - position.y,
+    });
+  };
+
+  const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+    if (dragging) {
+      setPosition({
+        x: e.clientX - offset.x,
+        y: e.clientY - offset.y,
+      });
+    }
+  };
+
+  const handleMouseUp = () => {
+    setDragging(false);
+  };
+
   return (
-    <Container>
-      <Raven />
-      <WaterDrop />
-      <Diamond />
-      <StrokeFull />
-      <Robot />
+    <Container onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}>
+      <DraggableIcon
+        ref={iconRef}
+        onMouseDown={handleMouseDown}
+        x={position.x}
+        y={position.y}
+      >
+        <Raven />
+        <WaterDrop />
+        <Diamond />
+        <StrokeFull />
+        <Robot />
+      </DraggableIcon>
     </Container>
   );
 };
