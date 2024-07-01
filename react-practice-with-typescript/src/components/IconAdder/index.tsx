89a8e70ef34906ddff8cdc721910b478f1753fe5
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-07-01 16:51:00 +0900

    [react-practice-with-typescript]feat: update IconAdder Component to generate multiple icons

--- react-practice-with-typescript/src/components/IconAdder/index.tsx
+++ react-practice-with-typescript/src/components/IconAdder/index.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   width: 500px;
   height: 300px;
   margin: 20px;
@@ -13,44 +14,66 @@ const Container = styled.div`
   border: 5px solid #ff7623;
 `;
 
+const IconBuilder = styled.div`
+  display: flex;
+  // gap: 5px;
+`;
+
 const Button = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 70px;
+  min-width: 70px;
   height: 50px;
   font-size: 30px;
   margin: 5px;
 `;
 
 const IconsContainer = styled.div`
+wie
   height: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 5px;
   font-size: 40px;
+  overflow: hidden;
+`;
+
+const IconComponent = styled.div`
+  flex-shrink: 0;
 `;
 
 export const IconAdder = () => {
   const [icons, setIcons] = useState<string[]>([]);
 
-  const addIcon = () => {
-    setIcons([...icons, "FcBriefcase"]);
+  const addIcon = (icon: string) => {
+    setIcons([...icons, icon]);
   };
 
   return (
     <Container>
-      <Button onClick={addIcon}>
-        +<FlatColorIcons.FcBriefcase />
-      </Button>
-      <IconsContainer>
-        {icons.map((icon, index) => {
-          const IconComponent =
-            FlatColorIcons[icon as keyof typeof FlatColorIcons];
-          return IconComponent ? <IconComponent key={index} /> : null;
-        })}
-      </IconsContainer>
+      <IconBuilder>
+        <Button onClick={() => addIcon("FcBriefcase")}>
+          +<FlatColorIcons.FcBriefcase />
+        </Button>
+        <IconsContainer>
+          {icons.map((icon, index) => {
+            const Icon = FlatColorIcons[icon as keyof typeof FlatColorIcons];
+            return <IconComponent key={index} as={Icon} />;
+          })}
+        </IconsContainer>
+      </IconBuilder>
+      <IconBuilder>
+        <Button onClick={() => addIcon("FcCloseUpMode")}>
+          +<FlatColorIcons.FcCloseUpMode />
+        </Button>
+      </IconBuilder>
+      <IconBuilder>
+        <Button onClick={() => addIcon("FcGlobe")}>
+          +<FlatColorIcons.FcGlobe />
+        </Button>
+      </IconBuilder>
     </Container>
   );
 };
