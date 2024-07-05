cd1fc8c38c315bd9a6a0af34a93733a54a58a7f5
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-07-05 23:16:52 +0900

    [react-practice]feat: add login form in Login component

--- react-practice/src/components/Login.jsx
+++ react-practice/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -17,6 +18,8 @@ const ContentsWrapper = styled.div`
 
 const ContentsContainer = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   min-width: 630px;
   min-height: 840px;
   padding: 20px;
@@ -26,12 +29,69 @@ const ContentsContainer = styled.div`
   margin: 10px;
 `;
 
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledInput = styled.input`
+  font-size: 20px;
+  height: 35px;
+  width: 300px;
+  padding: 5px 10px;
+  margin: 2px;
+  border: none;
+  border-radius: 5px;
+  box-sizing: border-box;
+`;
+
+const SubmitButton = styled.button`
+  font-size: 20px;
+  padding: 10px;
+  margin: 10px;
+  border: none;
+  background-color: tomato;
+  border-radius: 10px;
+  width: 300px;
+  box-sizing: border-box;
+`;
+
 export const Login = () => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "id") setId(value);
+    else if (name === "password") setPassword(value);
+  };
+
   return (
     <Container>
       <Spacer />
       <ContentsWrapper>
-        <ContentsContainer>Login Page</ContentsContainer>
+        <ContentsContainer>
+          <StyledForm>
+            <StyledInput
+              name="id"
+              type="text"
+              placeholder="Id"
+              value={id}
+              onChange={onChange}
+              required
+            />
+            <StyledInput
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={onChange}
+              required
+            />
+            <SubmitButton type="submit">Log In</SubmitButton>
+          </StyledForm>
+        </ContentsContainer>
       </ContentsWrapper>
     </Container>
   );
