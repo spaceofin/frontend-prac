a4bf79a09d44ce862fcfb43387408ec8b8c4c9e7
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-07-11 23:53:49 +0900

    [react-practice-with-typescript]feat: toggle box-shadow effect on Dog components when clicked

--- react-practice-with-typescript/src/components/ProfileGenerator/index.tsx
+++ react-practice-with-typescript/src/components/ProfileGenerator/index.tsx
@@ -4,6 +4,7 @@ import { ReactComponent as Dog2 } from "assets/icons/dog2.svg";
 import { ReactComponent as Dog3 } from "assets/icons/dog3.svg";
 import { ReactComponent as Dog4 } from "assets/icons/dog4.svg";
 import { ReactComponent as Dog5 } from "assets/icons/dog5.svg";
+import { useState } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -25,38 +26,50 @@ const DogsContainer = styled.div`
   margin: 20px 0px;
 `;
 
-const DogWrapper = styled.div`
+interface DogWrapperProps {
+  clicked: boolean;
+}
+
+const DogWrapper = styled.div<DogWrapperProps>`
   //   width: 200px;
   //   height: 200px;
   //   border: 5px solid orange;
   padding: 5px;
   margin: 2px;
   //   box-shadow: inset 5px 5px 0px orange, inset -5px -5px 0px orange;
-  box-shadow: inset 5px 0px 0px orange, inset -5px 0px 0px orange,
-    inset 0px 5px 0px orange, inset 0px -5px 0px orange;
+  box-shadow: ${(props) =>
+    props.clicked
+      ? "inset 5px 0px 0px orange, inset -5px 0px 0px orange, inset 0px 5px 0px orange, inset 0px -5px 0px orange"
+      : null};
   border-radius: 10px;
   //   z-index: 1;
 `;
 
 export const ProfileGenerator = () => {
-  const handleClick = () => {};
+  const [clicked, setClicked] = useState([false, false, false, false, false]);
+
+  const handleClick = (index: number) => {
+    const newClicked = [...clicked];
+    newClicked[index] = !newClicked[index];
+    setClicked(newClicked);
+  };
   return (
     <Container>
       Choose Dog for Profile
       <DogsContainer>
-        <DogWrapper>
+        <DogWrapper clicked={clicked[0]} onClick={() => handleClick(0)}>
           <Dog1 />
         </DogWrapper>
-        <DogWrapper>
+        <DogWrapper clicked={clicked[1]} onClick={() => handleClick(1)}>
           <Dog2 />
         </DogWrapper>
-        <DogWrapper>
+        <DogWrapper clicked={clicked[2]} onClick={() => handleClick(2)}>
           <Dog3 />
         </DogWrapper>
-        <DogWrapper>
+        <DogWrapper clicked={clicked[3]} onClick={() => handleClick(3)}>
           <Dog4 />
         </DogWrapper>
-        <DogWrapper>
+        <DogWrapper clicked={clicked[4]} onClick={() => handleClick(4)}>
           <Dog5 />
         </DogWrapper>
       </DogsContainer>
