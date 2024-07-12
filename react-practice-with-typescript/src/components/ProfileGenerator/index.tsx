3b642c2c26ffe0a60980535cfb10af6e95170381
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-07-12 15:32:32 +0900

    [react-practice-with-typescript]feat: enable profile image selection by clicking

--- react-practice-with-typescript/src/components/ProfileGenerator/index.tsx
+++ react-practice-with-typescript/src/components/ProfileGenerator/index.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as Dog2 } from "assets/icons/dog2.svg";
 import { ReactComponent as Dog3 } from "assets/icons/dog3.svg";
 import { ReactComponent as Dog4 } from "assets/icons/dog4.svg";
 import { ReactComponent as Dog5 } from "assets/icons/dog5.svg";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -39,22 +39,56 @@ const DogWrapper = styled.div<DogWrapperProps>`
   //   box-shadow: inset 5px 5px 0px orange, inset -5px -5px 0px orange;
   box-shadow: ${(props) =>
     props.clicked
-      ? "inset 5px 0px 0px orange, inset -5px 0px 0px orange, inset 0px 5px 0px orange, inset 0px -5px 0px orange"
+      ? "inset 5px 0px 0px lime, inset -5px 0px 0px green, inset 0px 5px 0px lime, inset 0px -5px 0px green"
       : null};
   border-radius: 10px;
   //   z-index: 1;
 `;
 
+const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 10px;
+  padding: 15px;
+  width: 450px;
+  height: 150px;
+  background-color: #cced71;
+  border-radius: 10px;
+`;
+
+const ProfileImageWrapper = styled.div`
+  width: 150px;
+  height: 150px;
+  border-radius: 10px;
+  background-color: white;
+`;
+
+const ProfileWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const LineText = styled.span`
+  font-family: cursive;
+  font-size: 20px;
+`;
+
 const dogComponents = [Dog1, Dog2, Dog3, Dog4, Dog5];
 
 export const ProfileGenerator = () => {
-  const [clicked, setClicked] = useState([false, false, false, false, false]);
+  const [clicked, setClicked] = useState<number | null>(null);
+  const [SelectedDog, setSelectedDog] = useState<React.ComponentType | null>(
+    null
+  );
 
   const handleClick = (index: number) => {
-    const newClicked = [...clicked];
-    newClicked[index] = !newClicked[index];
-    setClicked(newClicked);
+    setClicked(index);
+    setSelectedDog(dogComponents[index]);
   };
+
   return (
     <Container>
       Choose Dog for Profile
@@ -62,12 +96,20 @@ export const ProfileGenerator = () => {
         {dogComponents.map((Dog, index) => (
           <DogWrapper
             key={index}
-            clicked={clicked[index]}
+            clicked={clicked === index}
             onClick={() => handleClick(index)}>
             <Dog />
           </DogWrapper>
         ))}
       </DogsContainer>
+      <ProfileContainer>
+        <ProfileImageWrapper>
+          {SelectedDog && <SelectedDog />}
+        </ProfileImageWrapper>
+        <ProfileWrapper>
+          <LineText>Profile</LineText>
+        </ProfileWrapper>
+      </ProfileContainer>
     </Container>
   );
 };
