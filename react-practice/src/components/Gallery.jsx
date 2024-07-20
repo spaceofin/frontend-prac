50e508a482b8f44a517acc99487e1500e63ea01d
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-07-20 16:07:08 +0900

    [react-practice]feat: implement logout by setting user to null and redirecting to main page

--- react-practice/src/components/Gallery.jsx
+++ react-practice/src/components/Gallery.jsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { useCartPhotos } from "contexts/CartPhotosContext";
 import { useAuth } from "contexts/AuthContext";
+import { ReactComponent as AccountCircle } from "assets/icons/account-circle.svg";
+import { ReactComponent as Logout } from "assets/icons/logout.svg";
 
 const Container = styled.div`
   display: flex;
@@ -30,7 +32,8 @@ const PhotosContainer = styled.div`
 
 const ButtonWrapper = styled.div`
   display: flex;
-  justify-content: right;
+  // justify-content: right;
+  justify-content: flex-end;
   width: 670px;
   margin-left: 10px;
 `;
@@ -56,6 +59,24 @@ const Spacer = styled.div`
   height: inherit;
 `;
 
+const UserContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 650px;
+  font-size: 20px;
+  font-weight: 600;
+  padding: 10px;
+  margin-left: 10px;
+  gap: 5px;
+`;
+
+const StyledLogOut = styled(Logout)`
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
 export const Gallery = () => {
   const photosCount = 12;
   const [clickedPhotos, setClickedPhotos] = useState(
@@ -64,7 +85,7 @@ export const Gallery = () => {
     )
   );
   const { cartPhotos, setCartPhotos, setRandomNumber } = useCartPhotos();
-  const { user } = useAuth();
+  const { user, setUser } = useAuth();
 
   console.log(user);
   if (user === null) console.log("user is null");
@@ -115,9 +136,18 @@ export const Gallery = () => {
     );
   }, [cartPhotos]);
 
+  const handleLogout = () => {
+    setUser(null);
+    navigate("/");
+  };
+
   return (
     <Container>
-      user: {user}
+      <UserContainer>
+        <AccountCircle />
+        {user ? user : "No User Information"}
+        <StyledLogOut onClick={handleLogout} />
+      </UserContainer>
       <ButtonWrapper>
         <Button color="lightgray" onClick={handleReloadClick}>
           Reload Photos
