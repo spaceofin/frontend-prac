373528b6eaffb19a805eaf437c996b145ed3cbca
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-09-20 14:56:39 +0900

    [react-query-ts-practice]feat: implement button-triggered next page loading using useInfiniteQuery on comments

--- react-query-ts-practice/src/components/Comments.tsx
+++ react-query-ts-practice/src/components/Comments.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
 type Comment = {
   postId: number;
@@ -11,52 +11,70 @@ type Comment = {
 export function Comments() {
   const url = 'https://jsonplaceholder.typicode.com/comments';
 
-  const fetchData = async (url: string) => {
+  const fetchData = async (pageParam: number) => {
     try {
-      const response = await fetch(url);
+      const response = await fetch(url + '?_page=' + pageParam);
       return await response.json();
     } catch (error) {
       console.error('Fetch error:', error);
     }
   };
 
-  const { isPending, error, data, isFetching } = useQuery({
-    queryKey: ['commentsData'],
-    queryFn: () => fetchData(url),
-  });
+  const { data, fetchNextPage, hasNextPage, isFetching, isLoading, isError, error } =
+    useInfiniteQuery({
+      queryKey: ['commentsData'],
+      queryFn: ({ pageParam }) => fetchData(pageParam),
+      initialPageParam: 1,
+      getNextPageParam: (pages) => {
+        return pages.length + 1;
+      },
+      getPreviousPageParam: (pages) => {
+        return pages.length - 1;
+      },
+    });
 
-  if (isPending || isFetching) {
+  if (isFetching || isLoading) {
     return <div>Loading...</div>;
   }
 
-  if (error) {
+  if (isError) {
     return <div>Error: {error.message}</div>;
   }
 
   return (
     <div>
-      <h1 style={{ marginLeft: '10px' }}>Comments</h1>
-      {data.map((comment: Comment) => (
-        <div key={comment.id}>
-          <div
-            style={{
-              border: '1px solid black',
-              borderRadius: '2px',
-              margin: '10px',
-              padding: '5px',
-            }}
-          >
-            <>postId: {comment.postId}</>
-            <br />
-            <>name: {comment.name}</>
-            <> / email: {comment.email}</>
-            <br />
-            <br />
-            <strong>Comment</strong>
-            <div>{comment.body}</div>
-          </div>
-        </div>
-      ))}
+      <h1 style={{ marginLeft: '20px' }}>Comments</h1>
+      {data &&
+        data.pages.map((pageData) =>
+          pageData.map((comment: Comment) => (
+            <div key={comment.id}>
+              <div
+                style={{
+                  border: '1px solid black',
+                  borderRadius: '2px',
+                  margin: '10px',
+                  padding: '10px',
+                }}
+              >
+                <>postId: {comment.postId}</>
+                <br />
+                <>name: {comment.name}</>
+                <> / email: {comment.email}</>
+                <br />
+                <br />
+                <strong>Comment</strong>
+                <div>{comment.body}</div>
+              </div>
+            </div>
+          )),
+        )}
+      <button
+        onClick={() => fetchNextPage()}
+        disabled={!hasNextPage}
+        style={{ marginLeft: '10px', marginBottom: '30px', padding: '5px 30px', fontSize: '18px' }}
+      >
+        Load Next Page
+      </button>
     </div>
   );
 }
