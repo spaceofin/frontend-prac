e3cddb7776e43c888052cd719327e09502c8799a
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-09-24 20:58:39 +0900

    [react-query-ts-practice]feat: add 'ALL' button for fetching all comments and input with 'Search' button for fetching comments by post id

--- react-query-ts-practice/src/components/Comments.tsx
+++ react-query-ts-practice/src/components/Comments.tsx
@@ -1,5 +1,6 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useQuery, useInfiniteQuery } from '@tanstack/react-query';
 import InfiniteScroll from 'react-infinite-scroller';
+import { useState } from 'react';
 
 type Comment = {
   postId: number;
@@ -10,6 +11,10 @@ type Comment = {
 };
 
 export function Comments() {
+  const [inputValue, setInputValue] = useState<number>(1);
+  const [searchPostId, setSearchPostId] = useState<number>(1);
+  const [allButtonClicked, setAllButtonClicked] = useState(true);
+
   const url = 'https://jsonplaceholder.typicode.com/comments';
 
   const fetchData = async (pageParam: number) => {
@@ -33,6 +38,21 @@ export function Comments() {
       },
     });
 
+  const fetchPostComments = async (postId: number) => {
+    try {
+      const response = await fetch(url + '?postId=' + postId);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+  };
+
+  const { data: postCommentsData } = useQuery({
+    queryKey: ['postCommentsData', searchPostId],
+    queryFn: () => fetchPostComments(searchPostId),
+  });
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -41,45 +61,116 @@ export function Comments() {
     return <div>Error: {error.message}</div>;
   }
 
+  const handleAllButtonClick = () => {
+    setAllButtonClicked(true);
+  };
+
+  const handleSearchButtonClick = () => {
+    setAllButtonClicked(false);
+    setSearchPostId(inputValue);
+  };
+
   return (
     <div>
       <h1 style={{ marginLeft: '20px' }}>Comments</h1>
       {isFetching && <div>Fetching...</div>}
-      <InfiniteScroll
-        loadMore={() => {
-          if (!isFetching) {
-            fetchNextPage();
-          }
-        }}
-        hasMore={hasNextPage}
-      >
-        {data &&
-          data.pages.map(
-            (pageData) =>
-              pageData &&
-              pageData.data.map((comment: Comment) => (
-                <div key={comment.id}>
-                  <div
-                    style={{
-                      border: '1px solid black',
-                      borderRadius: '2px',
-                      margin: '10px',
-                      padding: '10px',
-                    }}
-                  >
-                    <>postId: {comment.postId}</>
-                    <br />
-                    <>name: {comment.name}</>
-                    <> / email: {comment.email}</>
-                    <br />
-                    <br />
-                    <strong>Comment</strong>
-                    <div>{comment.body}</div>
+      <div style={{ marginLeft: '20px', display: 'flex', alignItems: 'center' }}>
+        <button onClick={handleAllButtonClick} style={{ fontSize: '16px', padding: '0px 7px' }}>
+          ALL
+        </button>
+        <div style={{ marginLeft: '5px', display: 'flex', alignItems: 'center' }}>
+          <span
+            style={{
+              fontSize: '18px',
+              backgroundColor: 'skyblue',
+              padding: '0px 7px',
+              margin: '0px 3px 0px 5px',
+              borderRadius: '5px',
+              color: 'white',
+            }}
+          >
+            POST ID
+          </span>
+          <input
+            type="number"
+            min="1"
+            max="20"
+            value={inputValue}
+            onChange={(e) => {
+              setInputValue(Number(e.target.value));
+            }}
+            style={{ width: '50px', fontSize: '16px' }}
+          />
+          <button
+            onClick={handleSearchButtonClick}
+            style={{ fontSize: '16px', padding: '0px 7px', marginLeft: '2px' }}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      {allButtonClicked ? (
+        <InfiniteScroll
+          loadMore={() => {
+            if (!isFetching) {
+              fetchNextPage();
+            }
+          }}
+          hasMore={hasNextPage}
+        >
+          {data &&
+            data.pages.map(
+              (pageData) =>
+                pageData &&
+                pageData.data.map((comment: Comment) => (
+                  <div key={comment.id}>
+                    <div
+                      style={{
+                        border: '1px solid black',
+                        borderRadius: '2px',
+                        margin: '10px',
+                        padding: '10px',
+                      }}
+                    >
+                      <>postId: {comment.postId}</>
+                      <br />
+                      <>name: {comment.name}</>
+                      <> / email: {comment.email}</>
+                      <br />
+                      <br />
+                      <strong>Comment</strong>
+                      <div>{comment.body}</div>
+                    </div>
                   </div>
+                )),
+            )}
+        </InfiniteScroll>
+      ) : (
+        <>
+          {postCommentsData &&
+            postCommentsData.map((comment: Comment) => (
+              <div key={comment.id}>
+                <div
+                  style={{
+                    border: '1px solid black',
+                    borderRadius: '2px',
+                    margin: '10px',
+                    padding: '10px',
+                  }}
+                >
+                  <>postId: {comment.postId}</>
+                  <br />
+                  <>name: {comment.name}</>
+                  <> / email: {comment.email}</>
+                  <br />
+                  <br />
+                  <strong>Comment</strong>
+                  <div>{comment.body}</div>
                 </div>
-              )),
-          )}
-      </InfiniteScroll>
+              </div>
+            ))}
+        </>
+      )}
       {/* <button
         onClick={() => fetchNextPage()}
         disabled={!hasNextPage}
