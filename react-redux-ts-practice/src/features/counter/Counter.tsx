bbd05199cae8118b56d8334c9fcb8ae143fce34d
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-09-30 15:44:46 +0900

    [react-redux-ts-practice]feat: implement configurable delay duration for 'Add Async' button and update styles for Counter

--- react-redux-ts-practice/src/features/counter/Counter.tsx
+++ react-redux-ts-practice/src/features/counter/Counter.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import {
   decrement,
   increment,
@@ -8,60 +8,91 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+} from "./counterSlice";
+import styles from "./Counter.module.css";
 
 export function Counter() {
   const count = useAppSelector(selectCount);
   const dispatch = useAppDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const [incrementAmount, setIncrementAmount] = useState("2");
+  const [delayTime, setDelayTime] = useState("5");
 
   const incrementValue = Number(incrementAmount) || 0;
+  const delayTimeMS = Number(delayTime) * 1000;
 
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
+    <div className="flex w-full justify-between bg-purple-400 bg-opacity-30 p-10 rounded-md">
+      <div className="flex flex-col justify-end items-center">
+        <label className={styles.label}>VALUE</label>
         <input
           className={styles.textbox}
           aria-label="Set increment amount"
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
+
+        <label className={styles.label}>DELAY TIME</label>
+        <input
+          className={styles.textbox}
+          aria-label="Set async time amount"
+          value={delayTime}
+          onChange={(e) => setDelayTime(e.target.value)}
+        />
+      </div>
+      <div className="flex flex-col justify-start items-end">
+        <div className={styles.row}>
+          <div className={styles.value}>{count}</div>
+          <div className="flex flex-col">
+            <button
+              className={styles.button}
+              style={{ fontSize: "30px", height: "40px", paddingBottom: "5px" }}
+              aria-label="Increment value"
+              onClick={() => dispatch(increment())}>
+              +
+            </button>
+            <button
+              className={styles.button}
+              style={{
+                fontSize: "44px",
+                height: "40px",
+                paddingBottom: "10px",
+              }}
+              aria-label="Decrement value"
+              onClick={() => dispatch(decrement())}>
+              -
+            </button>
+          </div>
+        </div>
+        <div className={styles.row}>
+          <button
+            className={styles.button}
+            onClick={() => dispatch(incrementByAmount(incrementValue))}>
+            Add Amount
+          </button>
+        </div>
+        <div className={styles.row}>
+          <button
+            className={styles.asyncButton}
+            style={
+              {
+                "--transition-duration": `${delayTime}s`,
+              } as React.CSSProperties
+            }
+            onClick={() =>
+              dispatch(
+                incrementAsync({ amount: incrementValue, delay: delayTimeMS })
+              )
+            }>
+            Add Async
+          </button>
+        </div>
+        <div className={styles.row}>
+          <button
+            className={styles.button}
+            onClick={() => dispatch(incrementIfOdd(incrementValue))}>
+            Add If Odd
+          </button>
+        </div>
       </div>
     </div>
   );
