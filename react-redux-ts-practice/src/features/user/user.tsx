8b3b0ad4c74477368bb9936610567e669ddfdf26
spaceofin <80182521+spaceofin@users.noreply.github.com>
2024-10-08 19:30:32 +0900

    [react-redux-ts-practice]feat: add edit functionality for name an email in user feature

--- react-redux-ts-practice/src/features/user/user.tsx
+++ react-redux-ts-practice/src/features/user/user.tsx
@@ -7,67 +7,97 @@ export const User = () => {
   const [email, setEmail] = useState("");
   const dispatch = useAppDispatch();
   const { name: userName, email: userEmail } = useAppSelector(selectUser);
-  const [isSave, setIsSave] = useState(false);
+  const [isNameSave, setIsNameSave] = useState(false);
+  const [isEmailSave, setIsEmailSave] = useState(false);
 
   const handleSaveClick = () => {
     dispatch(setUser({ name: name, email: email }));
-    setIsSave(true);
+    setIsNameSave(true);
+    setIsEmailSave(true);
   };
 
-  const handleChangeClick = () => {};
+  const handleEditClick = (type: "name" | "email") => {
+    if (type === "name") {
+      setIsNameSave(false);
+    } else if (type === "email") {
+      setIsEmailSave(false);
+    }
+  };
+
+  const handleEditSaveClick = (type: "name" | "email") => {
+    if (type === "name") {
+      dispatch(changeName(name));
+      setIsNameSave(true);
+    } else if (type === "email") {
+      dispatch(changeEmail(email));
+      setIsEmailSave(true);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center w-full bg-orange-300 font-mono relative">
       <div className="flex w-full justify-center text-4xl font-bold my-5">
         User
       </div>
-      {isSave ? (
-        <div className="flex w-full">
-          <div className="flex flex-col my-5 mb-12 mr-5 pl-24 w-3/4">
-            <div className="flex items-center text-3xl h-10">
-              <label className="w-28">Name:</label>
-              {name}
-            </div>
-            <div className="flex items-center text-3xl h-10">
-              <label className="w-28">Email:</label>
-              {email}
-            </div>
-          </div>
-          <button
-            onClick={handleChangeClick}
-            className="w-24 h-24 bg-orange-500 my-3 rounded-xl text-2xl text-white">
-            Change
-          </button>
-        </div>
-      ) : (
-        <div className="flex w-full">
-          <div className="flex flex-col my-5 mb-12 mr-5 pl-24 w-3/4">
-            <div className="flex items-center text-3xl h-10">
-              <label className="w-28">Name:</label>
+      <div className="flex w-full">
+        <div className="flex flex-col my-5 mb-12 mr-5 pl-24 w-3/4">
+          <div className="flex items-center text-3xl h-10">
+            <label className="w-28">Name:</label>
+            {isNameSave ? (
+              <>{userName}</>
+            ) : (
               <input
                 type="text"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
-                className="w-3/4 ml-8 my-1 mx-1 text-xl rounded-md border-4 border-orange-800 px-2"
+                className="w-3/4 my-1 mx-1 text-xl rounded-md border-4 border-orange-800 px-2"
               />
-            </div>
-            <div className="flex items-center text-3xl h-10">
-              <label className="w-28">Email:</label>
+            )}
+          </div>
+
+          <div className="flex items-center text-3xl h-10">
+            <label className="w-28">Email:</label>
+            {isEmailSave ? (
+              <>{userEmail}</>
+            ) : (
               <input
                 type="text"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="ml-8 w-3/4 my-1 mx-1 text-xl rounded-md border-4 border-orange-800 px-2"
+                className="w-3/4 my-1 mx-1 text-xl rounded-md border-4 border-orange-800 px-2"
               />
-            </div>
+            )}
           </div>
+        </div>
+        {isNameSave && isEmailSave ? (
+          <div className="flex flex-col gap-1 my-5">
+            <button
+              onClick={() => handleEditClick("name")}
+              className="w-24 h-9 bg-orange-500 rounded-xl text-2xl text-white">
+              Edit
+            </button>
+            <button
+              onClick={() => handleEditClick("email")}
+              className="w-24 h-9 bg-orange-500 rounded-xl text-2xl text-white">
+              Edit
+            </button>
+          </div>
+        ) : (
           <button
-            onClick={handleSaveClick}
-            className="w-24 h-24 bg-orange-500 my-3 rounded-xl text-2xl text-white">
+            onClick={() => {
+              if (!isNameSave && !isEmailSave) {
+                handleSaveClick();
+              } else if (!isNameSave) {
+                handleEditSaveClick("name");
+              } else if (!isEmailSave) {
+                handleEditSaveClick("email");
+              }
+            }}
+            className="w-24 h-20 bg-orange-500 my-5 rounded-xl text-2xl text-white">
             SAVE
           </button>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
