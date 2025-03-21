0af41020e0656d7d35b016abe484e72bc92d8bbf
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-03-21 18:20:15 +0900

    [react-ts-components]feat: add Buttons component

--- react-ts-components/src/App.tsx
+++ react-ts-components/src/App.tsx
@@ -1,25 +1,9 @@
-import Button from "./components/tailwindcss/Button";
+import Buttons from "./components/displays/Buttons";
 
 function App() {
   return (
-    <div className="grid grid-cols-3 gap-2 items-end w-72">
-      <Button size="sm">Button</Button>
-      <Button>Button</Button>
-      <Button size="lg">Button</Button>
-      <Button variant="outline" size="sm">
-        Button
-      </Button>
-      <Button variant="outline">Button</Button>
-      <Button variant="outline" size="lg">
-        Button
-      </Button>
-      <Button variant="ghost" size="sm">
-        Button
-      </Button>
-      <Button variant="ghost">Button</Button>
-      <Button variant="ghost" size="lg">
-        Button
-      </Button>
+    <div className="flex gap-10">
+      <Buttons styleTool="tailwindcss" />
     </div>
   );
 }
---
+++ react-ts-components/src/components/displays/Buttons.tsx
@@ -0,0 +1,63 @@
+import { JSX, useEffect, useState } from "react";
+
+type StyleTool = "tailwindcss" | "styled-components" | "css-modules";
+
+type ButtonsProps = {
+  styleTool: StyleTool;
+};
+
+type ButtonProps = {
+  className?: string;
+  variant?: "default" | "outline" | "ghost";
+  size?: "sm" | "md" | "lg";
+} & React.ComponentProps<"button">;
+
+type ButtonComponent = (props: ButtonProps) => JSX.Element;
+
+const buttonPaths = {
+  tailwindcss: "../tailwindcss/Button.tsx",
+};
+
+const Buttons = ({ styleTool }: ButtonsProps) => {
+  const [Button, setButton] = useState<ButtonComponent | null>(null);
+
+  useEffect(() => {
+    const loadButton = async () => {
+      const buttonPath = buttonPaths[styleTool as keyof typeof buttonPaths];
+      if (buttonPath) {
+        const { default: buttonModule } = await import(buttonPath);
+        setButton(() => buttonModule);
+      }
+    };
+
+    loadButton();
+  }, []);
+
+  if (!Button) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="grid grid-cols-3 gap-2 items-end w-72">
+      <Button size="sm">Button</Button>
+      <Button>Button</Button>
+      <Button size="lg">Button</Button>
+      <Button variant="outline" size="sm">
+        Button
+      </Button>
+      <Button variant="outline">Button</Button>
+      <Button variant="outline" size="lg">
+        Button
+      </Button>
+      <Button variant="ghost" size="sm">
+        Button
+      </Button>
+      <Button variant="ghost">Button</Button>
+      <Button variant="ghost" size="lg">
+        Button
+      </Button>
+    </div>
+  );
+};
+
+export default Buttons;
